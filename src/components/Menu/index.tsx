c542a61b8153f43848fc68f9262eb0f4b57c0eae
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-01 10:14:37 -0300

    Feito a estilização do menu faltando acertar a responsividade para dispostivos móveis

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,12 +2,29 @@ import { Link } from "react-router-dom";
 
 export function Menu() {
   return (
-    <nav>
-        <Link to="/">Home</Link>
-        <Link to="/sobre">Sobre</Link>
-        <Link to="/contato">Contato</Link>
-        <Link to="/integrantes">Integrantes</Link>
-        <Link to="/faq">FAQ</Link>
+    <nav className="flex justify-center">
+      <ul className="bg-[#ffffff] flex items-center mt-[55px] rounded-full">
+        <li className="ml-4">
+          <Link to="/">
+            <img src="/logo.png" alt="Logo do CT-HC" className="h-10 m-2" />
+          </Link>
+        </li>
+        <li className="ml-15">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="ml-14">
+          <Link to="/sobre">Sobre</Link>
+        </li>
+        <li className="ml-14">
+          <Link to="/contato">Contato</Link>
+        </li>
+        <li className="ml-14">
+          <Link to="/integrantes">Integrantes</Link>
+        </li>
+        <li className="ml-14 mr-6">
+          <Link to="/faq">FAQ</Link>
+        </li>
+      </ul>
     </nav>
   )
 }
