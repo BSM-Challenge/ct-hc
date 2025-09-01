45cd19c958440d0e4f947ea5e9eee058635edb91
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-01 14:08:48 -0300

    Importado o componente Main no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header"
+import Main from "./components/Main"
 
 function App() {
 
   return (
     <div className="bg-[#C3D9FF] h-screen">
       <Header />
+      <Main />
     </div>
   )
 }
