409e7fb38956df2f0bf7cd706a28cdcd6b633881
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-01 14:23:34 -0300

    Adicionado o texto de bem-vindo

--- src/components/Section-Welcome/index.tsx
+++ src/components/Section-Welcome/index.tsx
@@ -1,7 +1,7 @@
 export default function SectionWelcome() {
     return (
         <section>
-
+            <h1 className="">Bem-vindo ao Centro de Treinamento do Hospital das Clínicas!</h1>
         </section>
     )
 }
