d0b419a3b5ca16e4deed7cbf3cc00eb59b8d1d64
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-01 14:10:23 -0300

    Importado o componente SectionWelcome no Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,9 @@
+import SectionWelcome from "../Section-Welcome";
+
 export default function Main() {
     return (
         <main>
-            
+            <SectionWelcome />
         </main>
     )
 }
