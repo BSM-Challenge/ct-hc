bfe2bb20eb073b496b1a46ec5440b77855fcb734
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-01 09:26:45 -0300

    Adicionado route Home no createBrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './routes/Home/index.tsx'
+
+const router = createBrowserRouter([
+  {path: "/", element: <App />, errorElement: <div>Erro!</div>, children: [
+    {path: "/", element: <Home />}
+  ]},
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
