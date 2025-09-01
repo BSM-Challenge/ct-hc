b226827459f2a5aec3bd87e46010a9f3d3409120
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-01 09:46:46 -0300

    Feito a criação dos links usando O Link do react-router-dom e adicionado os seguintes links: Home, Sobre, Contato, Integrantes e faq

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 export function Menu() {
   return (
     <nav>
-        
+        <Link to="/">Home</Link>
+        <Link to="/sobre">Sobre</Link>
+        <Link to="/contato">Contato</Link>
+        <Link to="/integrantes">Integrantes</Link>
+        <Link to="/faq">FAQ</Link>
     </nav>
   )
 }
