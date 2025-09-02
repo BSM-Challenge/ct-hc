b07a5dfbc93ee4bfaa457204a0400426e9615ab4
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-02 19:16:52 -0300

    Adicionado novos tamanhos no texto e no banner

--- src/components/Section-Welcome/index.tsx
+++ src/components/Section-Welcome/index.tsx
@@ -3,18 +3,18 @@ import { FaCircleArrowRight } from "react-icons/fa6";
 export default function SectionWelcome() {
   return (
     <section className="flex">
-      <div className="container-welcome w-1/2">
+      <div className="flex justify-center flex-col w-1/2 opacity-0 animate-fade-in mt-40">
         <h1
           className="text-[3.75rem] text-center text-[var(--dark-blue-title)] font-bold"
           style={{ fontFamily: "Inter" }}
         >
           Bem-vindo ao Centro de Treinamento do Hospital das Clínicas!
         </h1>
-        <p className="text-2xl ml-10 mt-8">
+        <p className="text-2xl ml-10 mt-8 indent-8">
           Um ambiente interativo criado para ajudar você a usar o Portal
           do Paciente HC de forma simples, segura e sem preocupações.
         </p>
-        <p className="text-2xl ml-10 mt-5">
+        <p className="text-2xl ml-10 mt-5 indent-8">
           Um tutorial passo a passo em tempo real com instruções destacadas para
           guiar cada ação.
         </p>
@@ -30,11 +30,11 @@ export default function SectionWelcome() {
           </a>
         </div>
       </div>
-      <div className="w-1/2 flex justify-center mt-[-5rem]">
+      <div className="w-1/2 flex justify-center">
         <img 
         src="/banner-ct.png" 
         alt="" 
-        className="h-[700px] mt-5 ml-10"
+        className="h-[700px] mt-15 ml-10 opacity-0 animate-fade-in"
         />
       </div>
     </section>
