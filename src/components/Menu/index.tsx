0814bd33c40af15cb7b53971ade831219a7361d7
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-02 09:49:36 -0300

    Adicionado novos tamanhos nos links do menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,22 +6,22 @@ export function Menu() {
       <ul className="bg-[#ffffff] flex items-center mt-[55px] rounded-full">
         <li className="ml-4">
           <Link to="/">
-            <img src="/logo.png" alt="Logo do CT-HC" className="h-10 m-2" />
+            <img src="/logo.png" alt="Logo do CT-HC" className="h-15 m-2" />
           </Link>
         </li>
-        <li className="ml-15">
+        <li className="ml-15 text-2xl">
           <Link to="/">Home</Link>
         </li>
-        <li className="ml-14">
+        <li className="ml-14 text-2xl">
           <Link to="/sobre">Sobre</Link>
         </li>
-        <li className="ml-14">
+        <li className="ml-14 text-2xl">
           <Link to="/contato">Contato</Link>
         </li>
-        <li className="ml-14">
+        <li className="ml-14 text-2xl">
           <Link to="/integrantes">Integrantes</Link>
         </li>
-        <li className="ml-14 mr-6">
+        <li className="ml-14 text-2xl mr-6">
           <Link to="/faq">FAQ</Link>
         </li>
       </ul>
