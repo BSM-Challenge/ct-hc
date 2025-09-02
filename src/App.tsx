575ec19e7559f7501289e4239e9a8bf7900199e1
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-01 23:09:34 -0300

    Adicionado a var --light-blue de cor de fundo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Main from "./components/Main"
 function App() {
 
   return (
-    <div className="bg-[#C3D9FF] h-screen">
+    <div className="bg-[var(--light-blue)] h-screen">
       <Header />
       <Main />
     </div>
