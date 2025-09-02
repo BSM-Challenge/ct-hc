8fed2767dfb87d6ce8fa4108a811a765a29bee7f
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-02 19:21:45 -0300

    Adicionado o bg --light-blue na SectionWelcome

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -3,7 +3,9 @@ import SectionWelcome from "../Section-Welcome";
 export default function Main() {
     return (
         <main>
-            <SectionWelcome />
+            <div className="bg-[var(--light-blue)] h-screen">
+                <SectionWelcome />
+            </div>
         </main>
     )
 }
