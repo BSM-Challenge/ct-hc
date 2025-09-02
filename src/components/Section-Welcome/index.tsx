ece1a99c138a8357acebebfcd37d28e492b046ba
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-02 09:50:24 -0300

    Feito a estilização do container-welcome e falta o container-image

--- src/components/Section-Welcome/index.tsx
+++ src/components/Section-Welcome/index.tsx
@@ -1,7 +1,36 @@
+import { FaCircleArrowRight } from "react-icons/fa6";
+
 export default function SectionWelcome() {
-    return (
-        <section>
-            <h1 className="">Bem-vindo ao Centro de Treinamento do Hospital das Clínicas!</h1>
-        </section>
-    )
+  return (
+    <section>
+      <div className="container-welcome w-1/2">
+        <h1
+          className="text-[3.75rem] text-center text-[var(--dark-blue-title)] font-bold"
+          style={{ fontFamily: "Inter" }}
+        >
+          Bem-vindo ao Centro de Treinamento do Hospital das Clínicas!
+        </h1>
+        <p className="text-2xl ml-10 mt-8">
+          Um ambiente interativo criado para ajudar você a usar o Portal
+          do Paciente HC de forma simples, segura e sem preocupações.
+        </p>
+        <p className="text-2xl ml-10 mt-5">
+          Um tutorial passo a passo em tempo real com instruções destacadas para
+          guiar cada ação.
+        </p>
+        <div className="flex justify-center mt-10">
+          <a
+            href=""
+            className="inline-flex items-center justify-center px-7 py-3 rounded-full bg-[var(--color-blue)] text-white"
+            style={{ fontFamily: "Inter", fontWeight: "600", boxShadow: "var(--shadow-blue)" }}
+          >
+            Clique aqui e <br />
+            Saiba mais!
+            <FaCircleArrowRight className="text-2xl ml-2" />
+          </a>
+        </div>
+      </div>
+      <div className="w-1/2"></div>
+    </section>
+  );
 }
