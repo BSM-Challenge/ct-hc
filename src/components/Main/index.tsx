e4044c2d3ba157aa93d737a4e3d9b6e9b107fad7
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-03 11:33:19 -0300

    Adicionado a página de sobre ainda está em construção

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,14 @@
+import SectionSobre from "../Section-Sobre";
 import SectionWelcome from "../Section-Welcome";
 
 export default function Main() {
     return (
         <main>
-            <div className="bg-[var(--light-blue)] h-screen">
+            <div id="home" className="bg-[var(--light-blue)] h-screen">
                 <SectionWelcome />
+            </div>
+            <div id="sobre" className="bg-[var(--color-white)] h-screen p-4">
+                <SectionSobre />
             </div>
         </main>
     )
