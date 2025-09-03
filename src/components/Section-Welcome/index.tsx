e3456cad0b6e01b655cea80bf9c4073f1479a901
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-03 07:30:54 -0300

    Feito a responsividade para dispositivos móveis agora será feito a responsividade do menu

--- src/components/Section-Welcome/index.tsx
+++ src/components/Section-Welcome/index.tsx
@@ -2,39 +2,40 @@ import { FaCircleArrowRight } from "react-icons/fa6";
 
 export default function SectionWelcome() {
   return (
-    <section className="flex">
-      <div className="flex justify-center flex-col w-1/2 opacity-0 animate-fade-in mt-40">
+    <section className="flex flex-col md: pt-50 lg:flex-row">
+      <div className="flex justify-center flex-col w-full gap-3 lg:w-1/2 opacity-0 animate-fade-in lg: px-4 lg:px-0">
         <h1
-          className="text-[3.75rem] text-center text-[var(--dark-blue-title)] font-bold"
+          className="text-2xl sm:text-3xl md:text-4xl lg:text-[2.30rem]  xl:text-[60px] text-center text-[var(--dark-blue-title)] font-bold"
           style={{ fontFamily: "Inter" }}
         >
           Bem-vindo ao Centro de Treinamento do Hospital das Clínicas!
         </h1>
-        <p className="text-2xl ml-10 mt-8 indent-8">
+        <p className="text-base sm:text-lg md:text-xl md:text-center lg:text-1xl mt-4 lg:mt-8 lg:ml-10 indent-4 lg:indent-8 text-justify lg:text-left  xl:text-2xl">
           Um ambiente interativo criado para ajudar você a usar o Portal
           do Paciente HC de forma simples, segura e sem preocupações.
         </p>
-        <p className="text-2xl ml-10 mt-5 indent-8">
+        <p className="text-base sm:text-lg md:text-xl lg:text-1xl mt-3 lg:mt-5 lg:ml-10 indent-4 lg:indent-8 text-justify lg:text-left xl:text-2xl">
           Um tutorial passo a passo em tempo real com instruções destacadas para
           guiar cada ação.
         </p>
-        <div className="flex justify-center mt-10">
+        <div className="flex justify-center mt-6 lg:mt-10">
           <a
             href=""
-            className="inline-flex items-center justify-center px-7 py-3 rounded-full bg-[var(--color-blue)] text-white"
+            className="inline-flex items-center justify-center px-5 py-2 lg:px-7 lg:py-3 rounded-full bg-[var(--color-blue)] text-white text-sm sm:text-base lg:text-lg"
             style={{ fontFamily: "Inter", fontWeight: "600", boxShadow: "var(--shadow-blue)" }}
           >
-            Clique aqui e <br />
+            Clique aqui e <br className="hidden sm:block" />
             Saiba mais!
-            <FaCircleArrowRight className="text-2xl ml-2" />
+            <FaCircleArrowRight className="text-lg sm:text-xl lg:text-2xl ml-2" />
           </a>
         </div>
       </div>
-      <div className="w-1/2 flex justify-center">
+
+      <div className="w-full lg:w-1/2 flex justify-center lg:-mt-50 px-4 lg:px-0">
         <img 
-        src="/banner-ct.png" 
-        alt="" 
-        className="h-[700px] mt-15 ml-10 opacity-0 animate-fade-in"
+          src="/banner-ct.png" 
+          alt="Banner Centro de Treinamento" 
+          className="w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-none lg:h-[600px] lg:mt-30 lg:ml-10 xl:h-[700px] xl:mt-15 opacity-0 animate-fade-in object-contain"
         />
       </div>
     </section>
