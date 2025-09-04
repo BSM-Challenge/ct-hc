c38b606c0ed592bff4d5547f33b657ae8ff8e920
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-04 10:06:10 -0300

    Removido os espaços que estava no código

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -4,10 +4,10 @@ import SectionWelcome from "../Section-Welcome";
 export default function Main() {
   return (
     <main>
-      <section id="home" className="bg-[var(--light-blue)] h-screen ">
+      <section id="home" className="bg-[var(--light-blue)] h-screen">
         <SectionWelcome />
       </section>
-      <section id="sobre" className="bg-[var(--color-white)] h-screen p-4 ">
+      <section id="sobre" className="bg-[var(--color-white)] h-screen p-4">
         <SectionSobre />
       </section>
     </main>
