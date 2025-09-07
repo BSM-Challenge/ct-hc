8ef2745802f058ad3f31d45d8ea895573aefb7d9
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-07 09:15:54 -0300

    Implementado a imagem de logo com texto

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,14 @@
 export default function Footer() {
     return (
-        <footer>Footer</footer>
+        <footer 
+         className="w-full bg-[var(--Color-blue-709CFF)]" 
+         style={{ fontFamily: "Inter" }}
+        >
+            <div className="px-5 py-4"> 
+                <figcaption>
+                    <img src="/logo-com-texto.png" alt="" />
+                </figcaption>
+            </div>
+        </footer>
     )
 }
