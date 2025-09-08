ce1bd01effa8f5b88b8c8ab571a821a2beb46a39
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-08 10:03:33 -0300

    Criado a rota para contato

---
+++ src/routes/Contato/index.tsx
@@ -0,0 +1,8 @@
+export default function Contato() {
+    return (
+        <section id="contato" className="bg-[var(--light-blue)] min-h-screen ">
+            <h1 className="text-4xl font-bold">Contate-nos!</h1>
+            <p>Ficou com alguma dúvida ou precisa de ajuda?</p>
+        </section>
+    )
+}
