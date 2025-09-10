591a13d2879b03833146e4cd9bf791875b0ba8a7
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-10 09:30:50 -0300

    Criado o componente FAQ e iniciado a sua construção com o título da página

---
+++ src/routes/FAQ/index.tsx
@@ -0,0 +1,16 @@
+export default function FAQ() {
+    return (
+        <main>
+            <section 
+            className="bg-[var(--light-blue)] pt-50 px-80 h-screen"
+            >
+                <div className="w-full h-10">
+                    <div className="h-[100%] flex justify-center items-center">
+                        <h1 className="text-6xl text-[var(--dark-blue-title)]">Perguntas Frequentes</h1>
+                    </div>
+                    <div></div>
+                </div>
+            </section>
+        </main>
+    )
+}
