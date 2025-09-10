78e7004c4313a4269e677eefa65771e9f7085c44
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-10 10:59:03 -0300

    Alterado os tamanhos dos textos no menu do faq

--- src/routes/FAQ/index.tsx
+++ src/routes/FAQ/index.tsx
@@ -4,9 +4,9 @@ export default function FAQ() {
   return (
     <main>
       <section className="bg-[var(--light-blue)] pt-40 px-20 min-h-screen">
-        <div className="w-full flex flex-col gap-4">
+        <div className="w-full flex flex-col gap-8">
           <div className="h-[100%] flex justify-center items-center">
-            <h1 className="text-6xl text-[var(--dark-blue-title)] font-bold">
+            <h1 className="text-5xl text-[var(--dark-blue-title)] font-bold">
               Perguntas Frequentes
             </h1>
           </div>
@@ -17,71 +17,73 @@ export default function FAQ() {
             >
               <ul className="flex flex-col gap-2 text-[var(--color-white)]">
                 <li className="border-b-2 border-[var(--color-white)]">
-                  <h2 className="text-2xl font-bold">Sobre o Projeto</h2>
+                  <h2 className="text-xl font-bold">Sobre o Projeto</h2>
                 </li>
                 <li className="ml-2">
-                  <Link to="" className="text-xl">
+                  <Link to="" className="text-[1.1rem]">
                     CT-HC e Portal HC
                   </Link>
                 </li>
                 <li className="ml-2">
-                  <Link to="" className="text-xl">
+                  <Link to="" className="text-[1.1rem]">
                     CT-HC gratuito
                   </Link>
                 </li>
               </ul>
               <ul className="flex flex-col gap-2 text-[var(--color-white)]">
                 <li className="border-b-2 border-[var(--color-white)]">
-                  <h2 className="text-2xl font-bold">Dados e Segurança</h2>
+                  <h2 className="text-xl font-bold">Dados e Segurança</h2>
                 </li>
                 <li className="ml-2">
-                  <Link to="" className="text-xl">
+                  <Link to="" className="text-[1.1rem]">
                     Dados do paciente
                   </Link>
                 </li>
                 <li className="ml-2">
-                  <Link to="" className="text-xl">
+                  <Link to="" className="text-[1.1rem]">
                     Informações pessoais
                   </Link>
                 </li>
                 <li className="ml-2">
-                  <Link to="" className="text-xl">
+                  <Link to="" className="text-[1.1rem]">
                     Informações no tutorial
                   </Link>
                 </li>
               </ul>
               <ul className="flex flex-col gap-2 text-[var(--color-white)]">
                 <li className="border-b-2 border-[var(--color-white)]">
-                  <h2 className="text-2xl font-bold">Uso do tutorial</h2>
+                  <h2 className="text-xl font-bold">Uso do tutorial</h2>
                 </li>
                 <li className="ml-2">
-                  <Link to="" className="text-xl">
+                  <Link to="" className="text-[1.1rem]">
                     Pular tutorial
                   </Link>
                 </li>
                 <li className="ml-2">
-                  <Link to="" className="text-xl">
+                  <Link to="" className="text-[1.1rem]">
                     Uso no celular
                   </Link>
                 </li>
                 <li className="ml-2">
-                  <Link to="" className="text-xl">
+                  <Link to="" className="text-[1.1rem]">
                     Repetir tutorial
                   </Link>
                 </li>
                 <li className="ml-2">
-                  <Link to="" className="text-xl">
+                  <Link to="" className="text-[1.1rem]">
                     Cria conta CT-HC
                   </Link>
                 </li>
                 <li className="ml-2">
-                  <Link to="" className="text-xl">
+                  <Link to="" className="text-[1.1rem]">
                     Uso em dispositivos móveis
                   </Link>
                 </li>
               </ul>
             </nav>
-            <div></div>
+            <div>
+                
+            </div>
           </div>
         </div>
       </section>
