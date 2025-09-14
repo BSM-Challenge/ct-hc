baf048e4a1e063c7eee5b789d505fc382975d566
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-14 18:08:15 -0300

    Adicionado o título da página e colocado a sua borda

--- src/routes/Integrantes/index.tsx
+++ src/routes/Integrantes/index.tsx
@@ -1,7 +1,14 @@
 export default function Integrantes() {
     return (
-        <div>
-            
-        </div>
+        <main className="pt-40 bg-[var(--light-blue)]">
+            <section className="min-h-screen">
+                <div className="flex flex-col items-center">
+                    <h1 className="text-5xl font-bold text-[var(--dark-blue-title)]">
+                        Conheça nossa equipe
+                    </h1>
+                    <div className="w-48 h-2 bg-[var(--color-blue)] mt-7 "></div>
+                </div>
+            </section>
+        </main>
     )
 }
