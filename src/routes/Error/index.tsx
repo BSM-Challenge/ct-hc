6b82e55bd2aaeb47219e910590be181405e446a5
Sofia Siqueira <ssofiasiq@gmail.com>
2025-09-16 17:57:38 -0300

    Atualizando a página de Erro adicionando mais conteúdo.

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -1,7 +1,10 @@
 export default function Error() {
     return (
         <main>
-            <h1>ERRO!</h1>
+            <h1>Ops!</h1>
+            <img src="./" alt="Imagem representando que a página não encontrada." />
+            <p>A página que você procurava não foi encontrada, ou não existe! Tente novamente ou clique no botão abaixo para voltar para a página inicial.</p>
+            <button>Voltar</button>
         </main>
     )
 }
