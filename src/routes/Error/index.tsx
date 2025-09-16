3122d0a419e9cad08333f28a2594d80f2b8d0ace
Sofia Siqueira <ssofiasiq@gmail.com>
2025-09-16 18:03:17 -0300

    Adicionando imagem e iniciando a estilização - Página de erro.

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -1,10 +1,16 @@
 export default function Error() {
     return (
         <main>
-            <h1>Ops!</h1>
-            <img src="./" alt="Imagem representando que a página não encontrada." />
-            <p>A página que você procurava não foi encontrada, ou não existe! Tente novamente ou clique no botão abaixo para voltar para a página inicial.</p>
+            <div className="w-full h-10 bg-[var(--Color-blue-709CFF)]"></div>
+            <div className="flex flex-col items-center">
+                <h1 className="text-9xl font-semibold text-[var(--dark-blue-title)]">Ops!</h1>
+                <img src="./error-icon.png" alt="Imagem representando que a página não encontrada." />
+                <p>A página que você procurava não foi encontrada, ou não existe! Tente novamente ou clique no botão abaixo para voltar para a página inicial.</p>
+            </div>
+            
             <button>Voltar</button>
+            <div className="w-full h-10 bg-[var(--Color-blue-709CFF)]"></div>
+
         </main>
     )
 }
