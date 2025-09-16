0b3cf566661e9e812db31622d409e9f7e1ddf039
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-15 23:23:44 -0300

    Definido quando a imagem vai ficar no início ou no final

--- src/routes/Integrantes/index.tsx
+++ src/routes/Integrantes/index.tsx
@@ -14,6 +14,7 @@ export default function Integrantes() {
           <Card
             nome="Moisés Barsoti Andrade de Oliveira"
             img="/public/foto-moises.jpg"
+            posicaoImg="left"
             descricao="Sou um estudante de Análise e Desenvolvimento de Sistemas na FIAP, com formação técnica em Desenvolvimento de Software pela Proz Educação. Apaixonado por tecnologia, tenho interesse em programação e desenvolvimento de soluções digitais, buscando sempre evoluir minhas habilidades e conhecimentos para atuar de forma criativa e eficiente na área de TI."
             rm="565049"
             turma="1TDSPG"
@@ -27,6 +28,7 @@ export default function Integrantes() {
           <Card
             nome="Sofia Siqueira Fontes"
             img="/public/foto-sofia.jpg"
+            posicaoImg="right"
             descricao="Estudante de Desenvolvimento de Sistemas na FIAP, apaixonada por tecnologia e motivada a explorar novas possibilidades. Tenho perfil proativo e gosto de compreender como as coisas funcionam, o que me impulsiona a buscar soluções criativas e eficazes. A cada dia desenvolvo minhas habilidades e conhecimentos para contribuir de forma significativa no universo digital."
             rm="563829"
             turma="1TDSPG"
@@ -40,6 +42,7 @@ export default function Integrantes() {
           <Card
             nome="Felipe Kirschner Modesto"
             img="/public/foto-felipe.jpg"
+            posicaoImg=""
             descricao="Sou aluno da Fiap, atualmente cursando Análise e Desenvolvimento de Sistemas. Desde quando ingressei-me no meio tecnológico, passei a admirar grandemente a área da programação, assim como diversos outros sistemas e tecnologias fundadas. O que me fez enxergar uma oportunidade de transformar ideias em soluções que façam a diferença. Tenho interesse em aprender e desenvolver constantemente minhas habilidades, para assim crescer tanto no ramo acadêmico, e me desenvolver profissionalmente."
             rm="561810"
             turma="1TDSPG"
