5d7d14fc75a2d2f7e4a07f1abc690c6b010cbfcc
Sofia Siqueira <ssofiasiq@gmail.com>
2025-09-16 19:14:51 -0300

    Adicionando efeito de hover no botão 'Voltar'.

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -8,7 +8,7 @@ export default function Error() {
                 <p className="text-[var(--dark-blue-title)] text-4xl text-center mb-23">A página que você procurava não foi encontrada, ou não existe! <br/>Tente novamente ou clique no botão abaixo para voltar para a página inicial.</p>
             </div>
             
-            <button className="text-[var(--color-blue)] rounded-4xl w-40 h-14 font-semibold text-3xl absolute bottom-12 right-4 cursor-pointer mb-5 mr-5 border-4 border-solid border-[var(--color-blue)] " onClick={() => window.location.href = '/'}>Voltar</button>
+            <button className="text-[var(--color-blue)] rounded-4xl w-40 h-14 font-semibold text-3xl absolute bottom-12 right-4 cursor-pointer mb-5 mr-5 border-4 border-solid border-[var(--color-blue)] hover:bg-[var(--light-blue-2)] hover:text-[var(--color-white)] hover:border-solid hover:border-[var(--Color-blue-709CFF)]" onClick={() => window.location.href = '/'}>Voltar</button>
             <div className="w-full h-10 bg-[var(--Color-blue-709CFF)]"></div>
 
         </main>
