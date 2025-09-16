2694e482a7abc4db32f70ed5d796bc8faa6882a6
Sofia Siqueira <ssofiasiq@gmail.com>
2025-09-16 19:12:21 -0300

    Estilização feita, faltando responsividade - Página de Erro

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -1,14 +1,14 @@
 export default function Error() {
     return (
-        <main>
+        <main className="relative ">
             <div className="w-full h-10 bg-[var(--Color-blue-709CFF)]"></div>
             <div className="flex flex-col items-center">
-                <h1 className="text-9xl font-semibold text-[var(--dark-blue-title)]">Ops!</h1>
-                <img src="./error-icon.png" alt="Imagem representando que a página não encontrada." />
-                <p>A página que você procurava não foi encontrada, ou não existe! Tente novamente ou clique no botão abaixo para voltar para a página inicial.</p>
+                <h1 className="mt-5 mb-15 text-9xl font-semibold text-[var(--dark-blue-title)]">Ops!</h1>
+                <img src="./error-icon.png" alt="Imagem representando que a página não encontrada." className="ml-20 mb-11 w-lg"/>
+                <p className="text-[var(--dark-blue-title)] text-4xl text-center mb-23">A página que você procurava não foi encontrada, ou não existe! <br/>Tente novamente ou clique no botão abaixo para voltar para a página inicial.</p>
             </div>
             
-            <button>Voltar</button>
+            <button className="text-[var(--color-blue)] rounded-4xl w-40 h-14 font-semibold text-3xl absolute bottom-12 right-4 cursor-pointer mb-5 mr-5 border-4 border-solid border-[var(--color-blue)] " onClick={() => window.location.href = '/'}>Voltar</button>
             <div className="w-full h-10 bg-[var(--Color-blue-709CFF)]"></div>
 
         </main>
