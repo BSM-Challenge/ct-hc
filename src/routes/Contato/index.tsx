2d372616d28b239cc5912eac619837ed3a3900b9
Sofia Siqueira <ssofiasiq@gmail.com>
2025-09-17 14:19:40 -0300

    Criado a primeira section da página de Contato.

--- src/routes/Contato/index.tsx
+++ src/routes/Contato/index.tsx
@@ -1,8 +1,33 @@
 export default function Contato() {
     return (
     <main className="bg-[var(--light-blue)]">
-        <h1>Contate-nos!</h1>
-        <h2>Ficou com alguma dúvida ou precisa de ajuda?</h2>
+        <section>
+            <h1>Contate-nos!</h1>
+            <h2>Ficou com alguma dúvida ou precisa de ajuda?</h2>
+            <div className="bg-[var(--light-blue-2)]">
+                <img src="" alt="Ícone de localização" />
+                <h2>Endereço - HC</h2>
+                <p>Av. Dr. Enéas de Carvalho Aguiar, <br /> Nº 255 <br /> Cerqueira César <br /> 05403-000 <br /> São Paulo - Brasil</p>
+
+                <img src="" alt="Ícone de telefone" />
+                <h2>Telefone - HC</h2>
+                <p>Telefone: (11) 2661-0000</p>
+
+                <img src="" alt="Ícone de WhatsApp" />
+                <h2>WhatsApp - HC</h2>
+                <p>E-mail:" </p>
+
+                <div className="border-[var(--dark-blue-title)] w-100 border-2 border-dotted"></div>
+                <h1>Siga o HC nas Redes Sociais:</h1>
+                <img src="" alt="Ícone do Instagram" />
+                <img src="" alt="Ícone do Facebook" />
+                <img src="" alt="Ícone do Twitter"/>
+                <img src="" alt="Ícone do Youtube"/>
+
+            </div>
+        </section>
+        
+        
     </main>
     );
 }
