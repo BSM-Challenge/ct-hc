651ee5a2c3b7024080f4c0a6dcc012f48b8c142b
Sofia Siqueira <ssofiasiq@gmail.com>
2025-09-17 18:50:56 -0300

    Responsividade da Página de Erro refeita.

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -6,32 +6,23 @@ export default function Error() {
       <header className="w-full h-10 bg-[var(--Color-blue-709CFF)]"></header>
     <main className="flex-grow">
       <div className="flex flex-col items-center gap-13">
-        <h1
-          className="
-          mt-5 mb-15 text-9xl font-semibold text-[var(--dark-blue-title)]
-          sm:mb-14
-          md:mb-13
-          lg:mt-10"
+        <h1 className="text-9xl font-semibold text-[var(--dark-blue-title)] mt-4"
         >
           Ops!
         </h1>
 
         <img src="./error-icon.png"
         alt="Imagem representando que a página não encontrada."
-        className="
-        ml-12 mb-11 w-80
-        sm:w-xs sm:mb-10
-        md:mb-3 md:w-95
-        xl:w-120"
+        className="ml-9 w-80
+        sm:w-xs md:w-95
+        2xl:w-120"
         />
 
-        <p className="
-        text-[var(--dark-blue-title)] text-4xl text-center mb-23 p-3
-        sm:text-3xl 
-        sm:p-8 sm:mb-8
-        md:mb-2
-        lg:p-15 lg:text-4xl
-        xl:text-5xl"
+        <p className="text-[var(--dark-blue-title)] text-4xl text-center mb-23 w-80
+        sm:w-150
+        lg:w-230
+        xl:w-300
+        2xl:w-400 2xl:text-5xl 2xl:mt-5"
         >
           A página que você procurava não foi encontrada, ou não existe! <br />
           Tente novamente ou clique no botão abaixo para voltar para a página
@@ -40,9 +31,11 @@ export default function Error() {
       </div>
       
       <div className="flex justify-end">
-        <Link className=" text-[var(--color-blue)] rounded-4xl w-40 h-14 font-semibold text-3xl bottom-12 right-4 cursor-pointer mb-5 mr-5 border-4 border-solid border-[var(--color-blue)] hover:bg-[var(--light-blue-2)] hover:text-[var(--color-white)] hover:border-solid hover:border-[var(--Color-blue-709CFF)]
-        md:mr-12
-        lg:mb-13" to="/">
+        <Link className="text-[var(--color-blue)] rounded-4xl px-10 py-2 font-semibold text-3xl
+          mb-5 right-4 cursor-pointer mr-5 border-4 border-solid border-[var(--color-blue)]
+          hover:bg-[var(--light-blue-2)] hover:text-[var(--color-white)] hover:border-solid
+          hover:border-[var(--Color-blue-709CFF)] transition-colors duration-200
+          " to="/">
           Voltar
         </Link>
       </div>
