b6945b09fd6b67874f69fc755d29766304173077
Sofia Siqueira <ssofiasiq@gmail.com>
2025-09-17 17:09:44 -0300

    Imagens adicionadas na segunda section da página de Contato. Conteúdo pronto, faltando estilização.

--- src/routes/Contato/index.tsx
+++ src/routes/Contato/index.tsx
@@ -5,24 +5,24 @@ export default function Contato() {
             <h1>Contate-nos!</h1>
             <h2>Ficou com alguma dúvida ou precisa de ajuda?</h2>
             <div className="bg-[var(--light-blue-2)]">
-                <img src="" alt="Ícone de localização" />
+                <img src="./icon-local.png" alt="Ícone de localização" />
                 <h2>Endereço - HC</h2>
                 <p>Av. Dr. Enéas de Carvalho Aguiar, <br /> Nº 255 <br /> Cerqueira César <br /> 05403-000 <br /> São Paulo - Brasil</p>
 
-                <img src="" alt="Ícone de telefone" />
+                <img src="./icon-telefone.png" alt="Ícone de telefone" />
                 <h2>Telefone - HC</h2>
                 <p>Telefone: (11) 2661-0000</p>
 
-                <img src="" alt="Ícone de WhatsApp" />
+                <img src="./icon-whatsapp.png" alt="Ícone de WhatsApp" />
                 <h2>WhatsApp - HC</h2>
                 <p>E-mail:" </p>
 
                 <div className="border-[var(--dark-blue-title)] w-100 border-2 border-dotted"></div>
                 <h1>Siga o HC nas Redes Sociais:</h1>
-                <img src="" alt="Ícone do Instagram" />
-                <img src="" alt="Ícone do Facebook" />
-                <img src="" alt="Ícone do Twitter"/>
-                <img src="" alt="Ícone do Youtube"/>
+                <img src="./instagram-icon.png" alt="Ícone do Instagram" />
+                <img src="./facebook-icon.png" alt="Ícone do Facebook" />
+                <img src="./twitter-icon.png" alt="Ícone do Twitter"/>
+                <img src="./youtube-icon.png" alt="Ícone do Youtube"/>
             </div>
         </section>
 
