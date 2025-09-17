7fbf4fc5a61fb3378b8d35a44fb63793837f274c
Sofia Siqueira <ssofiasiq@gmail.com>
2025-09-17 19:52:10 -0300

    Iniciando estilização da página de Contato.

--- src/routes/Contato/index.tsx
+++ src/routes/Contato/index.tsx
@@ -1,45 +1,55 @@
 export default function Contato() {
-    return (
-    <main className="bg-[var(--light-blue)]">
-        <section>
-            <h1>Contate-nos!</h1>
-            <h2>Ficou com alguma dúvida ou precisa de ajuda?</h2>
-            <div className="bg-[var(--light-blue-2)]">
-                <img src="./icon-local.png" alt="Ícone de localização" />
-                <h2>Endereço - HC</h2>
-                <p>Av. Dr. Enéas de Carvalho Aguiar, <br /> Nº 255 <br /> Cerqueira César <br /> 05403-000 <br /> São Paulo - Brasil</p>
+  return (
+    <main className="pt-40 bg-[var(--light-blue)] flex-grow">
+      <h1 className="text-6xl text-center text-[var(--dark-blue-title)] font-bold">
+        Contate-nos!
+      </h1>
+      <p className="text-3xl text-center text-[var(--dark-blue-title)] font-semibold mt-2">
+        Ficou com alguma dúvida ou precisa de ajuda?
+      </p>
 
-                <img src="./icon-telefone.png" alt="Ícone de telefone" />
-                <h2>Telefone - HC</h2>
-                <p>Telefone: (11) 2661-0000</p>
+      <div className="flex flex-row justify-center mt-10">
+        <section className="bg-[var(--light-blue-2)]">
+            <div className="flex  flex-col">
+              <img className="w-20" src="./icon-local.png" alt="Ícone de localização" />
+              <h2>Endereço - HC</h2>
+              <p>
+                Av. Dr. Enéas de Carvalho Aguiar, <br /> Nº 255 <br /> Cerqueira
+                César <br /> 05403-000 <br /> São Paulo Brasil
+              </p>
 
-                <img src="./icon-whatsapp.png" alt="Ícone de WhatsApp" />
-                <h2>WhatsApp - HC</h2>
-                <p>E-mail:" </p>
+              <img className="w-20" src="./icon-telefone.png" alt="Ícone de telefone" />
+              <h2>Telefone - HC</h2>
+              <p>Telefone: (11) 2661-0000</p>
 
-                <div className="border-[var(--dark-blue-title)] w-100 border-2 border-dotted"></div>
-                <h1>Siga o HC nas Redes Sociais:</h1>
+              <img className="w-20" src="./icon-whatsapp.png" alt="Ícone de WhatsApp" />
+              <h2>WhatsApp - HC</h2>
+              <p>WhatsApp: (19) 3521-2121 </p>
+
+            <div className="border-[var(--dark-blue-title)] w-100 border-2 border-dotted"></div>
+            <h2>Siga o HC nas Redes Sociais:</h2>
+            <div className="flex flex-row">
                 <img src="./instagram-icon.png" alt="Ícone do Instagram" />
                 <img src="./facebook-icon.png" alt="Ícone do Facebook" />
-                <img src="./twitter-icon.png" alt="Ícone do Twitter"/>
-                <img src="./youtube-icon.png" alt="Ícone do Youtube"/>
+                <img src="./twitter-icon.png" alt="Ícone do Twitter" />
+                <img src="./youtube-icon.png" alt="Ícone do Youtube" />
             </div>
+          </div>
         </section>
 
         <section className="bg-[var(--color-white)]">
-            <h1>Mande uma mensagem!</h1>
-            <form>
-                <label htmlFor="nome">Nome:</label>
-                <input type="text" placeholder="Digte o seu nome" />
-                <label htmlFor="email">E-mail:</label>
-                <input type="email" placeholder="Digte o seu e-mail" />
-                <label htmlFor="mensagem">Mensagem:</label>
-                <textarea placeholder="Digte sua mensagem..." />
-                <button>Enviar</button>
-            </form>
+          <h2>Mande uma mensagem!</h2>
+          <form>
+            <label htmlFor="nome">Nome:</label>
+            <input type="text" placeholder="Digte o seu nome" />
+            <label htmlFor="email">E-mail:</label>
+            <input type="email" placeholder="Digte o seu e-mail" />
+            <label htmlFor="mensagem">Mensagem:</label>
+            <textarea placeholder="Digte sua mensagem..." />
+            <button>Enviar</button>
+          </form>
         </section>
-        
-        
+      </div>
     </main>
-    );
+  );
 }
--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -4,26 +4,18 @@ export default function Error() {
   return (
     <div className="flex flex-col min-h-screen">
       <header className="w-full h-10 bg-[var(--Color-blue-709CFF)]"></header>
-    <main className="flex-grow">
-      <div className="flex flex-col items-center gap-13">
-        <h1 className="text-9xl font-semibold text-[var(--dark-blue-title)] mt-4"
-        >
-          Ops!
-        </h1>
-
+      <main className="flex-grow">
+        <div className="flex flex-col items-center gap-13">
+          <h1 className="text-9xl font-semibold text-[var(--dark-blue-title)] mt-4">
+            Ops!
+          </h1>
+        
         <img src="./error-icon.png"
         alt="Imagem representando que a página não encontrada."
-        className="ml-9 w-80
-        sm:w-xs md:w-95
-        2xl:w-120"
-        />
+        className="ml-9 w-80 sm:w-xs md:w-95 2xl:w-120"/>
 
         <p className="text-[var(--dark-blue-title)] text-4xl text-center mb-23 w-80
-        sm:w-150
-        lg:w-230
-        xl:w-300
-        2xl:w-400 2xl:text-5xl 2xl:mt-5"
-        >
+        sm:w-150 lg:w-230 xl:w-300 2xl:w-400 2xl:text-5xl 2xl:mt-">
           A página que você procurava não foi encontrada, ou não existe! <br />
           Tente novamente ou clique no botão abaixo para voltar para a página
           inicial.
@@ -32,15 +24,15 @@ export default function Error() {
       
       <div className="flex justify-end">
         <Link className="text-[var(--color-blue)] rounded-4xl px-10 py-2 font-semibold text-3xl
-          mb-5 right-4 cursor-pointer mr-5 border-4 border-solid border-[var(--color-blue)]
-          hover:bg-[var(--light-blue-2)] hover:text-[var(--color-white)] hover:border-solid
-          hover:border-[var(--Color-blue-709CFF)] transition-colors duration-200
-          " to="/">
+        mb-5 right-4 cursor-pointer mr-5 border-4 border-solid border-[var(--color-blue)]
+        hover:bg-[var(--light-blue-2)] hover:text-[var(--color-white)] hover:border-solid
+        hover:border-[var(--Color-blue-709CFF)] transition-colors duration-200
+        " to="/">
           Voltar
         </Link>
       </div>
-    </main>
-    <footer className="w-full h-10 bg-[var(--Color-blue-709CFF)] bottom-0"></footer>
+      </main>
+      <footer className="w-full h-10 bg-[var(--Color-blue-709CFF)] bottom-0"></footer>
     </div>
   );
 }
