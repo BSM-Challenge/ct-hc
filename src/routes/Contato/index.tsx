2c251f071ff9d940f42cae895f6fcb586028f550
Sofia Siqueira <ssofiasiq@gmail.com>
2025-09-17 11:03:12 -0300

    Iniciando a construção da página de Contato, e criando a rota.

---
+++ src/routes/Contato/index.tsx
@@ -0,0 +1,8 @@
+export default function Contato() {
+    return (
+    <main className="bg-[var(--light-blue)]">
+        <h1>Contate-nos!</h1>
+        <h2>Ficou com alguma dúvida ou precisa de ajuda?</h2>
+    </main>
+    );
+}
