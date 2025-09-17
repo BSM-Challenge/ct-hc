f817ddfcfdac76dee84aaffb90a49df276c95e7f
Sofia Siqueira <ssofiasiq@gmail.com>
2025-09-16 21:57:21 -0300

    Responsividade para dispositivos médios feita - página de Erro.

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -1,25 +1,29 @@
 export default function Error() {
   return (
-    <main className=" min-h-screen flex-grow">
+    <main className="min-h-screen flex-grow">
       <div className="w-full h-10 bg-[var(--Color-blue-709CFF)]"></div>
       <div className="flex flex-col items-center">
         <h1
           className="
           mt-5 mb-15 text-9xl font-semibold text-[var(--dark-blue-title)]
-            sm:mb-14"
+          sm:mb-14
+          md:mb-13"
         >
           Ops!
         </h1>
 
         <img src="./error-icon.png" alt="Imagem representando que a página não encontrada."
           className="
-          ml-15 mb-11
-          sm:w-xs sm:mb-10"
+          ml-12 mb-11 w-80
+          sm:w-xs sm:mb-10
+          md:mb-3 md:w-95"
         />
 
         <p className="
-        text-[var(--dark-blue-title)] text-4xl text-center mb-23
-        sm:text-3xl sm:p-3 sm:mb-15"
+        text-[var(--dark-blue-title)] text-4xl text-center mb-23 p-3
+        sm:text-3xl 
+        sm:p-8 sm:mb-8
+        md:mb-2 md:p-10"
         >
           A página que você procurava não foi encontrada, ou não existe! <br />
           Tente novamente ou clique no botão abaixo para voltar para a página
@@ -27,7 +31,7 @@ export default function Error() {
         </p>
       </div>
       
-      <div className=" flex justify-end">
+      <div className="flex justify-end">
         <button
         className="
         text-[var(--color-blue)] rounded-4xl w-40 h-14 font-semibold text-3xl bottom-12 right-4 cursor-pointer mb-5 mr-5 border-4 border-solid border-[var(--color-blue)] hover:bg-[var(--light-blue-2)] hover:text-[var(--color-white)] hover:border-solid hover:border-[var(--Color-blue-709CFF)]"
@@ -38,8 +42,7 @@ export default function Error() {
       </div>
 
       <div className="
-      w-full h-10 bg-[var(--Color-blue-709CFF)]
-      sm:fixed bottom-0"></div>
+      w-full h-10 bg-[var(--Color-blue-709CFF)] bottom-0"></div>
     </main>
   );
 }
