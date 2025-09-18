5a4651adf988ed644dc0f88435c5dbac97b19451
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-17 22:48:14 -0300

    Criado a estrutura de textos da section

--- src/components/Section-Pitch/index.tsx
+++ src/components/Section-Pitch/index.tsx
@@ -1,7 +1,13 @@
 export default function SectionPitch() {
     return (
-        <div>
-            <h1>Section Pitch</h1>
-        </div>
+        <>
+            <div className="flex flex-col gap-6">
+                <h4 className="text-7xl font-bold text-[var(--dark-blue-title)]">Vídeo Pitch</h4>
+                <p className="text-4xl w-[40ch]">
+                    Saiba o que é e como funciona o CT-HC e veja como nós o ajudamos a utilizar
+                    o Portal do Paciente HC de forma mais simples, segura e acessível.
+                </p>
+            </div>
+        </>
     )
 }
