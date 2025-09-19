f5b40feb6a9c5431d7a53ac24fb9a840f975d88a
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-18 22:56:45 -0300

    Feito a responsividade da SectionPitch

--- src/components/Section-Pitch/index.tsx
+++ src/components/Section-Pitch/index.tsx
@@ -2,12 +2,21 @@ export default function SectionPitch() {
   return (
     <section className="
     bg-[var(--color-white)] min-h-screen py-20 pl-9
-     flex flex-col gap-5"
+     flex flex-col gap-5
+     max-lg:pl-4"
      >
-      <h4 className="text-7xl font-bold text-[var(--dark-blue-title)]">
+      <h4 className="
+      text-7xl font-bold text-[var(--dark-blue-title)]
+      max-[444px]:text-5xl
+      ">
         Vídeo Pitch
       </h4>
-      <p className="text-4xl w-[40ch]">
+      <p className="
+      text-2xl w-[95%]
+      xl:w-[60%]
+      lg:text-4xl
+      max-[497px]:text-xl
+      ">
         Saiba o que é e como funciona o 
         <span className="font-bold text-[var(--dark-blue-title)]"> CT-HC </span>
         e veja como nós o ajudamos a utilizar o Portal do Paciente HC de forma mais 
@@ -16,7 +25,10 @@ export default function SectionPitch() {
       </p>
 
       <div className="flex justify-center">
-        <div className="w-[660px] max-w-full aspect-video rounded-2xl shadow-lg overflow-hidden">
+        <div className="
+        w-[660px] max-w-full aspect-video rounded-2xl shadow-lg overflow-hidden
+        max-md:w-[90%]
+        ">
             <video
             className="w-full h-full object-cover"
             src="/video-pitch.mp4"
@@ -25,19 +37,28 @@ export default function SectionPitch() {
         </div>
       </div>
 
-      <aside className="flex flex-col gap-4 w-[30%]">
-          <p className="text-2xl mt-4 text-[var(--dark-blue-title)]">
+      <aside className="
+      flex flex-col gap-4 w-[100%]
+      md:w-[40%] max-md:w-[75%]
+      ">
+          <p className="
+          text-2xl mt-4 text-[var(--dark-blue-title)]
+          max-[500px]:text-xl
+          ">
             Problemas para visualizar? <br />
             Assista no <span className="font-bold">YouTube</span>.
           </p>
           <a
             href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-            target="_blank"
             className="
-            w-[32%]
+            w-[50%]
             bg-[var(--dark-blue-title)] text-[var(--color-white)]
             py-2 text-center rounded-full font-bold
             shadow-[6px_6px_11px_var(--dark-blue-title)]
+            sm:w-[40%]
+            md:w-[40%]
+            lg:w-[38%]
+            xl:w-[32%]
             "
           >
             Clique aqui
