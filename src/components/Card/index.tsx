b21775f7a2499546666da0cede14b4385686488a
Magic06 <fekimodesto04@gmail.com>
2025-09-23 19:26:22 -0300

    Responsividade dos cards de integrantes criado.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -51,7 +51,7 @@ export default function Card({
           }`}
         >
           <img
-            className="w-[300px] h-[300px] object-cover rounded-full border-[8px] border-[var(--color-blue)] shadow-2xl"
+            className="w-40 h-40 sm:w-60 sm:h-60 lg:w-[300px] lg:h-[300px] object-cover rounded-full border-[6px] sm:border-[8px] border-[var(--color-blue)] shadow-2xl z-10"
             src={img}
             alt={`Foto de ${nome}`}
           />
@@ -63,30 +63,32 @@ export default function Card({
             posicaoImg === "right" ? "mr-36" : "ml-36"
           }`}
         >
-          <h2 className="text-4xl font-bold bg-[var(--color-blue)] -ml-5 px-4 py-2 rounded-[10px] inline-block text-center">
+          {/* Nome */}
+          <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold bg-[var(--color-blue)] -ml-5 px-4 py-2 rounded-[10px] inline-block text-center">
             {nome}
           </h2>
 
-          <p className="ml-4 mt-4 text-xl leading-relaxed">{descricao}</p>
+          {/* Descrição */}
+          <p className="ml-4 mt-4 sm:text-lg lg:text-xl leading-relaxed px-1 sm:px-4">{descricao}</p>
 
           {/* RM e Turma + Contato */}
-          <div className="flex justify-between max-w-[95%] ml-4 mt-10">
+          <div className="flex flex-col lg:flex-row justify-between gap-6 px-1 sm:px-4 max-w-[95%] ml-4 mt-10">
             <div>
-              <p className="text-xl">
+              <p className="text-base sm:text-lg lg:text-xl">
                 <span className="font-bold">RM: </span>
                 {rm}
               </p>
-              <p className="text-xl">
+              <p className="text-base sm:text-lg lg:text-xl">
                 <span className="font-bold">Turma: </span>
                 {turma}
               </p>
             </div>
 
             <div className="flex flex-col gap-4 items-center">
-              <span className="font-bold text-xl">
+              <span className="font-bold text-base sm:text-lg lg:text-xl">
                 Entre em contato comigo:
               </span>
-              <div className="flex gap-6">
+              <div className="flex gap-6 flex-wrap">
                 <a
                   href={linkGithub}
                   target="_blank"
