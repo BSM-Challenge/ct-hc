b6c7b224eb9a711bdd6ad57dc9215ce4a4515244
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-24 09:09:15 -0300

    Criado a estrutura inicial do AppHC

--- src/AppHC.tsx
+++ src/AppHC.tsx
@@ -1,15 +1,11 @@
-import Footer from "./components/CT-HC/Footer"
-import { Header } from "./components/CT-HC/Header"
 import { Outlet } from "react-router-dom"
 
 function AppHC() {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
       <div className="flex-grow">
         <Outlet />
       </div>
-      <Footer />
     </div>
   )
 }
