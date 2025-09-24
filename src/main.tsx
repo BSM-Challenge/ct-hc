c821300f8e8950f23a80de995086735d9f828d25
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-24 09:08:19 -0300

    Criado um path para o AppHC

--- src/main.tsx
+++ src/main.tsx
@@ -9,15 +9,32 @@ import Home from './routes//CT-HC/Home/index.tsx'
 import FAQ from './routes/CT-HC/FAQ/index.tsx'
 import Integrantes from './routes//CT-HC/Integrantes/index.tsx'
 import Contato from './routes/CT-HC/Contato/index.tsx'
+// HC
+import AppHC from './AppHC.tsx'
+import HomeHC from './routes/HC/Home/index.tsx'
 
 const router = createBrowserRouter([
-  {path: "/", element: <App />, errorElement: <Error />, children: [
-    {path: "/", element: <Home />}, 
-    {path: "/contato", element: <Contato />},
-    {path: "/integrantes", element: <Integrantes />}, 
-    {path: "/faq", element: <FAQ />}, 
-  ]},
-])
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/contato", element: <Contato /> },
+      { path: "/integrantes", element: <Integrantes /> },
+      { path: "/faq", element: <FAQ /> },
+    ],
+  },
+  {
+    path: "/hc",
+    element: <AppHC />,
+    errorElement: <Error />,
+    children: [
+      { path: "/hc", element: <HomeHC /> },
+    ],
+  },
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
