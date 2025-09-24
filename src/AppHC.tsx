b1b12895c1bcd69f7d7818522e71ee6be505b54f
Moisés Barsoti <moises.barsot2@gmail.com>
2025-09-24 09:36:36 -0300

    Feito a importação do HeaderHC

--- src/AppHC.tsx
+++ src/AppHC.tsx
@@ -1,8 +1,10 @@
+import HeaderHC from "./components/HC/Header";
 import { Outlet } from "react-router-dom"
 
 function AppHC() {
   return (
     <div className="min-h-screen flex flex-col">
+      <HeaderHC />
       <div className="flex-grow">
         <Outlet />
       </div>
