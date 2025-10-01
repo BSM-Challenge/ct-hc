a3ffe14377dd0f13e9b9424eb0b3c9604f24eaed
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-01 08:50:29 -0300

    Criado o componente TitleHC que possui um título e um subtítulo e importado o type TitleHCProps

---
+++ src/components/HC/TitleHC/index.tsx
@@ -0,0 +1,11 @@
+import type { TitleHCProps } from "../../../types/HC/title";
+
+export default function TitleHC( {title, subtitle}: TitleHCProps) {
+    return (
+        <div>
+            <h1 className="text-4xl text-[var(--color-blue)]">{title}</h1>
+            <hr className="my-3"/>
+            <h2 className="text-lg">{subtitle}</h2>
+        </div>
+    )
+}
