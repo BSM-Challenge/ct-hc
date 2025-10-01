2880d947312903faaa25acb240c19a5bc2a4790a
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-01 11:19:52 -0300

    Feito a importação do CardAcessibilidade

--- src/routes/HC/HomeHC/index.tsx
+++ src/routes/HC/HomeHC/index.tsx
@@ -1,11 +1,13 @@
+import CardAcessibilidade from "../../../components/HC/Card-Acessibilidade/indes";
 import CardTreinamento from "../../../components/HC/Card-Treinamento";
 import TitleHC from "../../../components/HC/TitleHC";
 
 export default function HomeHC() {
     return (
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-2">
             <TitleHC title="Acessibilidade" />
             <CardTreinamento />
+            <CardAcessibilidade />
         </div>
     )
 }
