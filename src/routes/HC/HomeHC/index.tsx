41a3c0c34012245f90f3e4d22bffb06f287b62ff
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-01 15:39:44 -0300

    Criado 3 cards de CardAcessibilidade

--- src/routes/HC/HomeHC/index.tsx
+++ src/routes/HC/HomeHC/index.tsx
@@ -4,10 +4,29 @@ import TitleHC from "../../../components/HC/TitleHC";
 
 export default function HomeHC() {
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-5">
             <TitleHC title="Acessibilidade" />
             <CardTreinamento />
-            <CardAcessibilidade />
+            <div className="flex flex-wrap gap-3 justify-between">
+                <CardAcessibilidade 
+                    img="img-acessibilidade.png"
+                    title="Acessibilidade"
+                    description="Veja mais recursos de acessibilidade disponíveis."
+                    link="/hc/recursos"
+                />
+                <CardAcessibilidade 
+                    img="img-voz.png"
+                    title="Navegação por Voz"
+                    description="Navegue pelo site com a sua voz, clique para ativar."
+                    link="/hc/recursos"
+                />
+                <CardAcessibilidade 
+                    img="img-aumentar-fonte.png"
+                    title="Aumentar Fonte"
+                    description="Aumente ou diminua o tamanho das letras."
+                    link="/hc/recursos"
+                />
+            </div>
         </div>
     )
 }
