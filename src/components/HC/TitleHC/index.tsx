6be0dddabc4d7a8009334dd0a81bf7303c7753c3
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-01 19:20:03 -0300

    Adicionado o font-bold no h1

--- src/components/HC/TitleHC/index.tsx
+++ src/components/HC/TitleHC/index.tsx
@@ -3,7 +3,7 @@ import type { TitleHCProps } from "../../../types/HC/title";
 export default function TitleHC( {title, subtitle}: TitleHCProps) {
     return (
         <div>
-            <h1 className="text-4xl text-[var(--color-blue)]">{title}</h1>
+            <h1 className="text-4xl text-[var(--color-blue)] font-bold">{title}</h1>
             <hr className="my-3"/>
             <h2 className="text-lg">{subtitle}</h2>
         </div>
