a7dba8acbf0c20f8b0ebeb2d017abe9808b8e208
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-01 08:14:26 -0300

    Feito a responsividade do menu para Desktops e Notebooks

--- src/components/HC/Header/index.tsx
+++ src/components/HC/Header/index.tsx
@@ -4,7 +4,14 @@ import { IoPersonCircle } from "react-icons/io5";
 
 export default function HeaderHC() {
   return (
-    <header className="h-screen w-[20%] px-[20px]">
+    <header 
+    className="
+    h-screen w-[20%] px-[20px]
+    md:w-[30%]
+    lg:w-[27%]
+    xl:w-[21.5%]
+    "
+    >
       <div className="flex items-center">
         <Link to="/hc">
           <img src="/logoHC.png" alt="" className="w-[90%]" />
@@ -14,10 +21,10 @@ export default function HeaderHC() {
           title="Fechar Menu"
         />
       </div>
-      <nav className="mt-[3%]">
+      <nav className="mt-[1%]">
         <Link to="/hc">
           <ul>
-            <li className="flex justify-between items-center max-w-[70%] mb-4">
+            <li className="flex gap-6 items-center max-w-[100%] mb-4">
               <IoPersonCircle className="text-[var(--color-grey)] text-6xl" />
               <p className="text-[18px]">Olá, Moisés</p>
             </li>
@@ -26,7 +33,7 @@ export default function HeaderHC() {
         <hr className="my-3" />
         <div className="flex flex-col gap-4">
           <Link to="/hc">
-            <ul className="flex gap-6 items-center max-w-[80%]">
+            <ul className="flex gap-6 items-center max-w-[100%]">
               <li>
                 <img src="/icon-meus-resultados.png" alt="" className="w-[33px]" />
               </li>
@@ -36,7 +43,7 @@ export default function HeaderHC() {
             </ul>
           </Link>
           <Link to="/hc">
-            <ul className="flex gap-6 items-center max-w-[80%]">
+            <ul className="flex gap-6 items-center max-w-[100%]">
               <li>
                 <img src="/icon-receitas.png" alt="" className="w-[33px]" />
               </li>
@@ -46,7 +53,7 @@ export default function HeaderHC() {
             </ul>
           </Link>
           <Link to="/hc">
-            <ul className="flex gap-6 items-center max-w-[80%]">
+            <ul className="flex gap-6 items-center max-w-[100%]">
               <li>
                 <img src="/icon-minha-agenda.png" alt="" className="w-[33px]" />
               </li>
@@ -56,7 +63,7 @@ export default function HeaderHC() {
             </ul>
           </Link>
           <Link to="/hc">
-            <ul className="flex gap-6 items-center max-w-[80%]">
+            <ul className="flex gap-6 items-center max-w-[100%]">
               <li>
                 <img src="/icon-teleconsulta.png" alt="" className="w-[33px]" />
               </li>
@@ -66,7 +73,7 @@ export default function HeaderHC() {
             </ul>
           </Link>
           <Link to="/hc">
-            <ul className="flex gap-6 items-center max-w-[90%]">
+            <ul className="flex gap-6 items-center max-w-[100%]">
               <li>
                 <img src="/icon-solicitacao-de-exames.png" alt="" className="w-[33px]" />
               </li>
@@ -76,7 +83,7 @@ export default function HeaderHC() {
             </ul>
           </Link>
           <Link to="/hc">
-            <ul className="flex gap-6 items-center max-w-[80%]">
+            <ul className="flex gap-6 items-center max-w-[100%]">
               <li>
                 <img src="/icon-documentos.png" alt="" className="w-[33px]" />
               </li>
@@ -86,7 +93,7 @@ export default function HeaderHC() {
             </ul>
           </Link>
           <Link to="/hc">
-            <ul className="flex gap-6 items-center max-w-[90%]">
+            <ul className="flex gap-6 items-center max-w-[100%]">
               <li>
                 <img src="/icon-pesquisa-de-satisfacao.png" alt="" className="w-[33px]" />
               </li>
@@ -96,7 +103,7 @@ export default function HeaderHC() {
             </ul>
           </Link>
           <Link to="/hc">
-            <ul className="flex gap-6 items-center max-w-[80%]">
+            <ul className="flex gap-6 items-center max-w-[100%]">
               <li>
                 <img src="/icon-termos-de-aceite.png" alt="" className="w-[33px]" />
               </li>
@@ -106,7 +113,7 @@ export default function HeaderHC() {
             </ul>
           </Link>
           <Link to="/hc">
-            <ul className="flex gap-6 items-center max-w-[80%]">
+            <ul className="flex gap-6 items-center max-w-[100%]">
               <li>
                 <img src="/icon-preciso-de-ajuda.png" alt="" className="w-[33px]" />
               </li>
@@ -116,7 +123,7 @@ export default function HeaderHC() {
             </ul>
           </Link>
           <Link to="/hc">
-            <ul className="flex gap-6 items-center max-w-[80%]">
+            <ul className="flex gap-6 items-center max-w-[100%]">
               <li>
                 <img src="/icon-acessibilidade.png" alt="" className="w-[33px]" />
               </li>
@@ -127,7 +134,7 @@ export default function HeaderHC() {
           </Link>
         </div>
       </nav>
-      <hr className="my-4"/>
+      <hr className="my-2"/>
       <Link to="/">
         <ul className="flex gap-6 items-center">
           <li>
