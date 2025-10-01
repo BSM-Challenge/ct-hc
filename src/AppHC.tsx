a4ad477af981039596c549ccb519407ea371fcfd
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-01 15:40:11 -0300

    Alterado o tamanho do padding

--- src/AppHC.tsx
+++ src/AppHC.tsx
@@ -5,7 +5,7 @@ function AppHC() {
   return (
     <div className="min-h-screen flex bg-[var(--background)]">
       <HeaderHC />
-      <div className="flex-grow bg-white rounded-[40px] my-3 mr-3 px-[25px] pt-6 pb-10">
+      <div className="flex-grow bg-white rounded-[40px] my-3 mr-3 px-[25px] py-6">
         <Outlet />
       </div>
     </div>
