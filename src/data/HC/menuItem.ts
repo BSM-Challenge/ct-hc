a5642ebf99292878e79ab461caf185c755e00c15
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-11 18:08:34 -0300

    feat(menuItem): Alterado a rota de acessibilidade para hc

--- src/data/HC/menuItem.ts
+++ src/data/HC/menuItem.ts
@@ -9,5 +9,5 @@ export const menuItems: MenuItem[] = [
     { to: "/documentos", icon: "/icon-documentos.png", label: "Documentos" },
     { to: "/pesquisa", icon: "/icon-pesquisa-de-satisfacao.png", label: "Pesquisa de Satisfação" },
     { to: "/termos", icon: "/icon-termos-de-aceite.png", label: "Termos de Aceite" },
-    { to: "/acessibilidade", icon: "/icon-acessibilidade.png", label: "Acessibilidade" },
+    { to: "/hc", icon: "/icon-acessibilidade.png", label: "Acessibilidade" },
 ]
