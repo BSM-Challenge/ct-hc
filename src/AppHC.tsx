43e1a82c814d03153c56be7dc1e83f834bd6f2cc
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-12 17:46:11 -0300

    feat(AppHC): Aplicado a responsividade no container para dispositivos: tablets e celulares.

--- src/AppHC.tsx
+++ src/AppHC.tsx
@@ -5,7 +5,10 @@ function AppHC() {
   return (
     <div className="min-h-screen flex bg-[var(--background)]">
       <HeaderHC />
-      <div className="flex-grow bg-white rounded-[40px] my-3 mr-3 px-[25px] py-6">
+      <div className="
+      flex-grow bg-white rounded-[40px] my-3 mr-3 px-[25px] py-6
+      max-lg:rounded-none max-lg:my-0 max-lg:mr-0 max-lg:mb-20
+      ">
         <Outlet />
       </div>
     </div>
