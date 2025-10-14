75ea4c85c2cd5f4c7993a11e32a44d0468ef0fa9
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-13 23:10:08 -0300

    feat(MaisRecursos): Adicionado o componente TitleHC e passado sua propriedade correspondente

--- src/routes/HC/MaisRecursos/index.tsx
+++ src/routes/HC/MaisRecursos/index.tsx
@@ -1,5 +1,12 @@
+import TitleHC from "../../../components/HC/TitleHC";
+
 export default function MaisRecursos() {
     return (
-        <div></div>
+        <div>
+            <TitleHC 
+                title="Acessibilidade > Mais recursos"
+                subtitle="Saiba o que cada funcionalidade faz, e torne sua experiência mais acessível!"
+            />
+        </div>
     )
 }
