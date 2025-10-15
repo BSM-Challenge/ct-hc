e3f7f40e2c8dfbd7cd30826f8229218c663f8a72
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-15 10:27:33 -0300

    feat(MaisRecursos): Adicionado os valores das props nos cards. E feito umma troca de display de flex para grid

--- src/routes/HC/MaisRecursos/index.tsx
+++ src/routes/HC/MaisRecursos/index.tsx
@@ -8,10 +8,25 @@ export default function MaisRecursos() {
                 title="Acessibilidade > Mais recursos"
                 subtitle="Saiba o que cada funcionalidade faz, e torne sua experiência mais acessível!"
             />
-            <div className="flex justify-between">
-                <CardMaisRecursos />
-                <CardMaisRecursos />
-                <CardMaisRecursos />
+            <div className="grid grid-cols-3 gap-4">
+                <CardMaisRecursos 
+                    titleMessage="Clique aqui para aumentar a fonte"
+                    img="/img-aumentar-fonte-2.png"
+                    title="Aumentar Fonte"
+                    text="Amplia ou diminui o tamanho do texto para facilitar a leitura."
+                />
+                <CardMaisRecursos 
+                    titleMessage="Clique aqui para ativar a navegação por Voz"
+                    img="/img-navegacao-voz.png"
+                    title="Navegação por Voz"
+                    text="Navegue pelo site usando comandos de voz."
+                />
+                <CardMaisRecursos 
+                    titleMessage="Clique aqui para mudar o contraste"
+                    img="/img-aumentar-fonte-2.png"
+                    title="Mudar contraste"
+                    text="Ajusta o contraste da tela para melhorar a visibilidade."
+                />
             </div>
         </div>
     )
