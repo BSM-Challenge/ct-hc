206a8d213cac8e134bfe4d9211eb29c768682f97
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-15 08:58:21 -0300

    feat(MaisRecursos): Importado o componente CardMaisRecursos

--- src/routes/HC/MaisRecursos/index.tsx
+++ src/routes/HC/MaisRecursos/index.tsx
@@ -1,12 +1,18 @@
+import CardMaisRecursos from "../../../components/HC/CardMaisRecursos";
 import TitleHC from "../../../components/HC/TitleHC";
 
 export default function MaisRecursos() {
     return (
-        <div>
+        <div className="flex flex-col gap-15">
             <TitleHC 
                 title="Acessibilidade > Mais recursos"
                 subtitle="Saiba o que cada funcionalidade faz, e torne sua experiência mais acessível!"
             />
+            <div className="flex justify-between">
+                <CardMaisRecursos />
+                <CardMaisRecursos />
+                <CardMaisRecursos />
+            </div>
         </div>
     )
 }
