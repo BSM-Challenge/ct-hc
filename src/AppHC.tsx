0b83b9ef107e94f28252cff4aa57f0b493ac53f1
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-16 09:29:31 -0300

    refactor(AppHC): troca de <div> por <main> para melhor semântica

--- src/AppHC.tsx
+++ src/AppHC.tsx
@@ -5,12 +5,12 @@ function AppHC() {
   return (
     <div className="min-h-screen flex bg-[var(--background)]">
       <HeaderHC />
-      <div className="
+      <main className="
       flex-grow bg-white rounded-[40px] my-3 mr-3 px-[25px] py-6
       max-lg:rounded-none max-lg:my-0 max-lg:mr-0 max-lg:mb-20
       ">
         <Outlet />
-      </div>
+      </main>
     </div>
   )
 }
