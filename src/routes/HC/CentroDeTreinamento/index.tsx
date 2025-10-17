c7747fa8e84c5ce7190d6dc058aea837ec47dc7a
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-17 08:06:21 -0300

    feat(CentroDeTreinamento): Adicionado o componente TitleHC

--- src/routes/HC/CentroDeTreinamento/index.tsx
+++ src/routes/HC/CentroDeTreinamento/index.tsx
@@ -1,7 +1,12 @@
+import TitleHC from "../../../components/HC/TitleHC";
+
 export default function CentroDeTreinamento() {
     return (
         <section>
-            CentroDeTreinamento
+            <TitleHC
+                title="Centro de Treinamento"
+                subtitle="Escolha a área do site que deseja fazer o treinamento e receba instruções em cada etapa:"
+             />
         </section>
     )
 }
