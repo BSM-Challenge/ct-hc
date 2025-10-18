ab0e82a85ca0b1cd064a152462be4a71723a8e78
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-18 08:44:12 -0300

    feat(CentroDeTreinamento): Feito a responsividade dos cards para todos os dispositivos

--- src/routes/HC/CentroDeTreinamento/index.tsx
+++ src/routes/HC/CentroDeTreinamento/index.tsx
@@ -4,19 +4,24 @@ import { iconsTreinamento } from "../../../data/HC/iconsTreinamento";
 
 export default function CentroDeTreinamento() {
     return (
-        <section className="flex flex-col gap-6">
+        <section className="flex flex-col gap-16 max-lg:gap-10">
             <TitleHC
                 title="Centro de Treinamento"
                 subtitle="Escolha a área do site que deseja fazer o treinamento e receba instruções em cada etapa:"
              />
-            <ul className="w-full flex flex-wrap justify-center gap-6 px-30">
+            <ul className="
+            w-full flex flex-wrap justify-center gap-6 xl:px-20 2xl:px-50 
+            max-[474px]:justify-start
+            "
+            >
                 {iconsTreinamento.map((i) => (
                     <li
                         key={i.label}
                         className="
                         bg-[var(--light-blue)] rounded-[10px]
                         flex justify-center items-center py-4 min-w-[200px]
-                        hover:bg-[var(--light-blue-2)] duration-300"
+                        hover:bg-[var(--light-blue-2)] duration-300
+                        max-[474px]:w-full"
                     >
                     <Link to={i.link || ""} title={i.titleMessage}>
                         <figure className="flex flex-col items-center gap-2">
