2ad22b1880890a8d9fdd15fa0fb475a4403ea841
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-18 17:15:24 -0300

    fix(iconsTreinamento.ts): Feito a correção do label incorreto de agendamentos
    O label estava escrito 'meus agendas' e foi alterado para 'minhas agendas'.

--- src/data/HC/iconsTreinamento.ts
+++ src/data/HC/iconsTreinamento.ts
@@ -3,7 +3,7 @@ import type { IconsTreinamentoProps } from "../../types/HC/iconTreinamento";
 export const iconsTreinamento: IconsTreinamentoProps[] = [
     {img: 'icon-resultados-azul.png', link: '', label: 'Meus Resultados', titleMessage: 'Clique para iniciar o treinamento dos meus resultados'},
     {img: 'icon-receitas-azul.png', link: '', label: 'Minhas Receitas', titleMessage: 'Clique para iniciar o treinamento das minhas receitas'},
-    {img: 'icon-agendamentos-azul.png', link: '', label: 'Meus Agendas', titleMessage: 'Clique para iniciar o treinamento dos meus agendamentos'},
+    {img: 'icon-agendamentos-azul.png', link: '', label: 'Minhas Agendas', titleMessage: 'Clique para iniciar o treinamento dos meus agendamentos'},
     {img: 'icon-teleconsulta-azul.png', link: '', label: 'Teleconsulta', titleMessage: 'Clique para iniciar o treinamento da teleconsulta'},
     {img: 'icon-exames-azul.png', link: '', label: 'Solicitação de Exames', titleMessage: 'Clique para iniciar o treinamento de solicitação de exames'},
     {img: 'icon-documentos-azul.png', link: '', label: 'Documentos', titleMessage: 'Clique para iniciar o treinamento de documentos'},
