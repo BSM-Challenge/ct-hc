cf5bf7c4191e258f07c7e51b25d1c6f26345e789
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-19 10:36:21 -0300

    feat(ModalTreinamentoFinalizado): Adicionado o useRef no modal
    Está sendo utilizado o useRef para controlar o modal e começado a base de criação do conteudo.

--- src/components/HC/ModalTreinamentoFinalizado/index.tsx
+++ src/components/HC/ModalTreinamentoFinalizado/index.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useRef } from "react";
+
 export default function ModalTreinamentoFinalizado() {
-    return (
-        <dialog>
-            
-        </dialog>
-    )
+  const dialog = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    dialog.current?.showModal();
+  }, []);
+
+  const fecharModal = () => dialog.current?.close();
+
+  return (
+    <dialog
+      ref={dialog}
+      className="
+            dialog-modal 
+            min-w-250 p-6 rounded-2xl 
+            inset-0 m-auto
+            flex justify-between
+        "
+    >
+      <div className="
+      w-1/2 bg-amber-200
+      flex justify-center
+      ">
+        <h2>Fim do tutorial!</h2>
+      </div>
+      <div className="w-1/2 bg-amber-300">
+        <figure>
+            <img src="" alt="" />
+        </figure>
+      </div>
+    </dialog>
+  );
 }
