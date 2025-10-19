890284018a579a895994a0e1102d5c12ef4f9c0b
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-19 15:56:34 -0300

    feat(ButtonCinza): criado o componente ButtonCinza

---
+++ src/components/HC/ButtonCinza/index.tsx
@@ -0,0 +1,18 @@
+import type { ButtonCinzaProps } from "../../../types/HC/buttonCinza";
+
+export default function ButtonCinza({onClick, px = 4, py = 1.5, titleMessage, children}: ButtonCinzaProps) {
+    return (
+        <button
+            className={`
+            bg-[var(--color-grey-818181)] text-[var(--color-gray)]
+            px-${px} py-${py} rounded-[10px] border-2 border-[var(--color-grey)]
+            hover:bg-[var(--color-grey)] duration-300 outline-none
+            hover:text-[var(--color-white)] cursor-pointer
+            `}
+            onClick={onClick}
+            title={titleMessage}
+            >
+            {children}
+        </button>
+    )
+}
