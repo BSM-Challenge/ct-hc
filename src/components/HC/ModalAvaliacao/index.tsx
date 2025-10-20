800c178300bdeea4b498bfb63fdbc3f39d67419c
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-20 10:26:23 -0300

    feat(ModalAvaliacao): feito a responsividade do modal
    Realizado a responsividade para dispositivos pequenos e somente faltando resolver o problema do hover dos emojis que não aparece a cor do emoji quando passa o cursor

--- src/components/HC/ModalAvaliacao/index.tsx
+++ src/components/HC/ModalAvaliacao/index.tsx
@@ -37,17 +37,27 @@ export default function ModalAvaliacao() {
             min-w-150 px-15 py-7 rounded-2xl 
             inset-0 m-auto outline-none
             flex flex-col items-center gap-3
+            max-md:px-10
+            max-sm:min-w-90 max-sm:px-5
+            max-[448px]:min-w-20
         "
     >
         <h2 className="text-4xl font-bold text-[var(--color-blue)]">
             Avalie-nos!
         </h2>
-        <p className="text-xl">Como foi sua experiência durante o tutorial?</p>
-        <ul className="flex gap-10 mt-6">
+        <p className="text-xl max-sm:text-lg">Como foi sua experiência durante o tutorial?</p>
+        <ul className="
+        flex gap-10 mt-6 max-[491px]:flex-col
+        max-[491px]:w-full max-[491px]:justify-start
+        ">
             {avaliacoes.map((avaliacao, index) => (
               <li key={index}>
                 <figure
-                  className="flex flex-col items-center gap-1 cursor-pointer"
+                  className="
+                  flex flex-col items-center gap-1 cursor-pointer
+                  max-[491px]:flex-row max-[491px]:gap-3
+                   
+                  "
                   title={avaliacao.titleMessage}
                   onClick={() => setSelectedIndex(index)}
                 >
@@ -56,8 +66,11 @@ export default function ModalAvaliacao() {
                     className={`text-4xl duration-200 ${
                       selectedIndex === index
                         ? avaliacao.activeColor
-                        : "text-[var(--color-white-emoji)] hover:" + avaliacao.hoverColor
-                    }`}
+                        : `text-[var(--color-white-emoji)] hover:${avaliacao.hoverColor}`
+                    }
+                    max-sm:text-2xl
+                    
+                    `}
                   >
                     {avaliacao.icon}
                   </div>
@@ -67,7 +80,7 @@ export default function ModalAvaliacao() {
             ))}
         </ul>
         <div className="flex flex-col w-full mt-10 gap-2">
-          <h3 className="text-xl">Gostaria de deixar um comentário?</h3>
+          <h3 className="text-xl max-[448px]:text-lg">Gostaria de deixar um comentário?</h3>
           <textarea
           className="
           resize-none w-full max-h-12 px-2 py-2 rounded-[10px] outline-none
@@ -77,14 +90,18 @@ export default function ModalAvaliacao() {
           placeholder="Digite uma mensagem">
           </textarea>
         </div>
-        <ul className="flex justify-around w-full mt-5">
+        <ul className="
+        flex justify-around w-full mt-5
+        max-[448px]:flex-col max-[448px]:gap-3
+        max-[448px]:justify-center max-[448px]:w-full
+        ">
           <li>
             <ButtonCinza
               onClick={fecharModal}
               px={10}
               py={2}
               titleMessage="Cancelar avaliação"
-              className="text-2xl"
+              className="text-2xl max-[448px]:w-full"
             >
               Cancelar
             </ButtonCinza>
@@ -96,6 +113,7 @@ export default function ModalAvaliacao() {
                   px-10 py-2 rounded-[10px] border-2 border-[var(--color-blue)]
                   hover:bg-[var(--color-blue)] duration-300 outline-none font-bold
                   text-2xl cursor-pointer
+                  max-[448px]:w-full
                   "
                 title="Enviar avaliação"
                 onClick={fecharModal}
