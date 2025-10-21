630a0fe0ae7be1e19c75c125c96999b5bc4df4d9
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-21 09:41:42 -0300

    refactor: apagado os arquivos desnecessários

--- src/data/HC/avaliacoes.ts
+++
@@ -1,9 +0,0 @@
-import type { AvaliacoesProps } from "../../types/HC/avaliacoes";
-
-export const avaliacoes: AvaliacoesProps[] = [
-    {img: '/emoji-bravo.png', text: 'Não gostei', titleMessage: "Cliqeue para avaliar como não gostei"},
-    {img: '/emoji-triste.png', text: 'Ruim', titleMessage: "Cliqeue para avaliar como ruim"},
-    {img: '/emoji-mais-ou-menos.png', text: 'Regular', titleMessage: "Cliqeue para avaliar como regular"},
-    {img: '/emoji-feliz.png', text: 'Bom', titleMessage: "Cliqeue para avaliar como bom"},
-    {img: '/emoji-otimo.png', text: 'Ótimo', titleMessage: "Cliqeue para avaliar como ótimo"},
-]
