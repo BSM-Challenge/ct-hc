9dded6d768ab17ec048d9426f58e624b775ac961
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-21 15:42:38 -0300

    fix(data/menuItem): alterado a rota dos links do menu
    Alterado a rota para /hc/NomeDaPagina

--- src/data/HC/menuItem.tsx
+++ src/data/HC/menuItem.tsx
@@ -8,56 +8,56 @@ export interface MenuItem {
 
 export const menuItems: MenuItem[] = [
     { 
-        to: "/resultados",
+        to: "/hc/resultados",
         icon: "/icon-meus-resultados.png",
         label: "Meus Resultados",
         title: "Ver Resultados",  
         activeColor: "text-[var(--color-blue)]"
     },
     { 
-        to: "/receitas",
+        to: "/hc/receitas",
         icon: "/icon-receitas.png",
         label: "Minhas Receitas",
         title: "Ver Minhas Receitas",  
         activeColor: "text-[var(--color-blue)]"
     },
     { 
-        to: "/agenda",
+        to: "/hc/agenda",
         icon: "/icon-minha-agenda.png",
         label: "Minhas Agendas",
         title: "Ver Minha Agenda",  
         activeColor: "text-[var(--color-blue)]"
     },
     { 
-        to: "/teleconsulta",
+        to: "/hc/teleconsulta",
         icon: "/icon-teleconsulta.png",
         label: "Teleconsulta",
         title: "Ver a Teleconsulta",  
         activeColor: "text-[var(--color-blue)]"
     },
     { 
-        to: "/exames",
+        to: "/hc/exames",
         icon: "/icon-solicitacao-de-exames.png",
         label: "Solicitação de Exames",
         title: "Ver a solicitação de exames",  
         activeColor: "text-[var(--color-blue)]"
     },
     { 
-        to: "/documentos",
+        to: "/hc/documentos",
         icon: "/icon-documentos.png",
         label: "Documentos",
         title: "Ver os Documentos",  
         activeColor: "text-[var(--color-blue)]"
     },
     { 
-        to: "/pesquisa",
+        to: "/hc/pesquisa",
         icon: "/icon-pesquisa-de-satisfacao.png",
         label: "Pesquisa de Satisfação",
         title: "Ver a pesquisa de satisfação",  
         activeColor: "text-[var(--color-blue)]"
     },
     { 
-        to: "/termos",
+        to: "/hc/termos",
         icon: "/icon-termos-de-aceite.png",
         label: "Termos de Aceite",
         title: "Ver os termos de Aceite",  
