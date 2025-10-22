97f36d67a1cc3da8d9febf400627e9b3decba5af
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-22 08:16:19 -0300

    feat(MeusResultados): adicionado o componente TitleHC

--- src/routes/HC/MeusResultados/index.tsx
+++ src/routes/HC/MeusResultados/index.tsx
@@ -1,5 +1,13 @@
+import TitleHC from "../../../components/HC/TitleHC";
+
 export default function MeusResultados() {
     return (
-        <section>resultados</section>
+        <section>
+            <TitleHC 
+            title="Meus Resultados"
+            font="normal" 
+            tamanho="2xl"
+             />
+        </section>
     )
 }
