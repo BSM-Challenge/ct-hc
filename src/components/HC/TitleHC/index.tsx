0db27ec395578722e21bd8b9d8d405edc0372582
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-22 08:03:35 -0300

    enhancement(TitleHC): adicionadas as props: tamanho, font e line

--- src/components/HC/TitleHC/index.tsx
+++ src/components/HC/TitleHC/index.tsx
@@ -1,10 +1,10 @@
 import type { TitleHCProps } from "../../../types/HC/title";
 
-export default function TitleHC( {title, subtitle}: TitleHCProps) {
+export default function TitleHC( {title, subtitle, tamanho='2xl', font='normal', line = false}: TitleHCProps) {
     return (
         <div>
-            <h1 className="text-4xl text-[var(--color-blue)] font-bold">{title}</h1>
-            <hr className="my-3"/>
+            <h1 className={`text-${tamanho} text-[var(--color-blue)] font-${font}`}>{title}</h1>
+            { line && <hr className="my-3"/> }
             <h2 className="text-lg">{subtitle}</h2>
         </div>
     )
