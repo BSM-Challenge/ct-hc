14d1ea0a6a369bb60fdc2206b6604697dacfc309
Sofia Siqueira <ssofiasiq@gmail.com>
2025-10-25 14:01:00 -0300

    enhancement (FAQ): Atualizando responsividade da página de FAQ.
    Responsividade da primeira section para telas pequenas feita.

--- src/routes/CT-HC/FAQ/index.tsx
+++ src/routes/CT-HC/FAQ/index.tsx
@@ -23,7 +23,7 @@ export default function FAQ() {
   return (
     <main className="bg-[var(--light-blue)]">
       <section
-        className="pt-30 min-h-screen max-[500px]:px-5
+        className="px-10 pt-30 min-h-screen max-[500px]:px-5
       lg:pt-35 
       xl:pt-45"
       >
@@ -33,7 +33,7 @@ export default function FAQ() {
           xl:justify-center"
           >
             <h1
-              className="text-3xl text-[var(--dark-blue-title)] font-bold mb-15
+              className="text-4xl text-center text-[var(--dark-blue-title)] font-bold mb-5
             sm:text-4xl 
             xl:text-5xl"
             >
@@ -41,13 +41,13 @@ export default function FAQ() {
             </h1>
           </div>
           <div
-            className="flex flex-col justify-between gap-10 items-start w-full
+            className="flex flex-col justify-between gap-20 items-center w-full
           lg:flex-row
           xl:flex-row"
           >
-            <div className="w-full lg:w-[30%] xl:w-[25%]">
+            <div className="w-full lg:w-[30%] xl:w-[25%] flex justify-center">
               <nav
-                className="bg-[var(--color-blue-2)] rounded-[20px] rounded-tl-[0] rounded-bl-[0]
+                className="bg-[var(--color-blue-2)] rounded-[20px]
               p-6 text-[var(--color-white)] flex flex-col gap-4 w-[70%]"
               >
                 <ul className="flex flex-col gap-[.6rem] text-[var(--color-white)]">
@@ -398,3 +398,410 @@ export default function FAQ() {
     </main>
   );
 }
+
+
+
+
+
+
+// // -----------------------------------------------------------------------
+// import { useState } from "react";
+// import { Link } from "react-router-dom";
+// import { FaAngleDown } from 'react-icons/fa';
+// import { faqData } from '../../../data/CT-HC/faqData';
+
+// export default function FAQ() {
+//   const [openItem, setOpenItem] = useState<string | null>(null);
+
+//   const toggleItem = (id: string) => {
+//     setOpenItem(openItem === id ? null : id);
+//   };
+
+//   const handleLinkClick = (id: string, elementId: string) => {
+//     toggleItem(id);
+//     setTimeout(() => {
+//       const element = document.getElementById(elementId);
+//       if (element) {
+//         element.scrollIntoView();
+//       }
+//     }, 100);
+//   };
+
+//   return (
+//     <main className="bg-[var(--light-blue)]">
+//       <section
+//         className="pt-30 min-h-screen max-[500px]:px-5
+//       lg:pt-35 
+//       xl:pt-45"
+//       >
+//         <div className="w-full flex flex-col gap-5">
+//           <div
+//             className="h-full flex justify-center items-center 
+//           xl:justify-center"
+//           >
+//             <h1
+//               className="text-3xl text-[var(--dark-blue-title)] font-bold mb-15
+//             sm:text-4xl 
+//             xl:text-5xl"
+//             >
+//               Perguntas Frequentes
+//             </h1>
+//           </div>
+//           <div
+//             className="flex flex-col justify-between gap-10 items-start w-full
+//           lg:flex-row
+//           xl:flex-row"
+//           >
+//             <div className="w-full lg:w-[30%] xl:w-[25%]">
+//               <nav
+//                 className="bg-[var(--color-blue-2)] rounded-[20px] rounded-tl-[0] rounded-bl-[0]
+//               p-6 text-[var(--color-white)] flex flex-col gap-4 w-[70%]"
+//               >
+//                 <ul className="flex flex-col gap-[.6rem] text-[var(--color-white)]">
+//                   <li className="border-b-2 border-[var(--color-white)]">
+//                     <h2 className="text-xl font-bold">Sobre o Projeto</h2>
+//                   </li>
+//                   <li className="ml-2">
+//                     <Link
+//                       to="#sobre-0"
+//                       className="text-[1.1rem] hover:underline"
+//                       onClick={(e) => {
+//                         e.preventDefault();
+//                         handleLinkClick("0-0", "sobre-0");
+//                       }}
+//                     >
+//                       CT-HC e Portal HC
+//                     </Link>
+//                   </li>
+//                   <li className="ml-2">
+//                     <Link
+//                       to="#sobre-1"
+//                       className="text-[1.1rem] hover:underline"
+//                       onClick={(e) => {
+//                         e.preventDefault();
+//                         handleLinkClick("0-1", "sobre-1");
+//                       }}
+//                     >
+//                       CT-HC gratuito
+//                     </Link>
+//                   </li>
+//                 </ul>
+//                 <ul className="flex flex-col gap-[.6rem] text-[var(--color-white)]">
+//                   <li className="border-b-2 border-[var(--color-white)]">
+//                     <h2 className="text-xl font-bold">Dados e Segurança</h2>
+//                   </li>
+//                   <li className="ml-2">
+//                     <Link
+//                       to="#dados-0"
+//                       className="text-[1.1rem] hover:underline"
+//                       onClick={(e) => {
+//                         e.preventDefault();
+//                         handleLinkClick("1-0", "dados-0");
+//                       }}
+//                     >
+//                       Dados do paciente
+//                     </Link>
+//                   </li>
+//                   <li className="ml-2">
+//                     <Link
+//                       to="#dados-1"
+//                       className="text-[1.1rem] hover:underline"
+//                       onClick={(e) => {
+//                         e.preventDefault();
+//                         handleLinkClick("1-1", "dados-1");
+//                       }}
+//                     >
+//                       Informações pessoais
+//                     </Link>
+//                   </li>
+//                   <li className="ml-2">
+//                     <Link
+//                       to="#dados-2"
+//                       className="text-[1.1rem] hover:underline"
+//                       onClick={(e) => {
+//                         e.preventDefault();
+//                         handleLinkClick("1-2", "dados-2");
+//                       }}
+//                     >
+//                       Informações no tutorial
+//                     </Link>
+//                   </li>
+//                 </ul>
+//                 <ul className="flex flex-col gap-[.6rem] text-[var(--color-white)]">
+//                   <li className="border-b-2 border-[var(--color-white)]">
+//                     <h2 className="text-xl font-bold">Uso do tutorial</h2>
+//                   </li>
+//                   <li className="ml-2">
+//                     <Link
+//                       to="#tutorial-0"
+//                       className="text-[1.1rem] hover:underline"
+//                       onClick={(e) => {
+//                         e.preventDefault();
+//                         handleLinkClick("2-0", "tutorial-0");
+//                       }}
+//                     >
+//                       Pular tutorial
+//                     </Link>
+//                   </li>
+//                   <li className="ml-2">
+//                     <Link
+//                       to="#tutorial-1"
+//                       className="text-[1.1rem] hover:underline"
+//                       onClick={(e) => {
+//                         e.preventDefault();
+//                         handleLinkClick("2-1", "tutorial-1");
+//                       }}
+//                     >
+//                       Uso no celular
+//                     </Link>
+//                   </li>
+//                   <li className="ml-2">
+//                     <Link
+//                       to="#tutorial-2"
+//                       className="text-[1.1rem] hover:underline"
+//                       onClick={(e) => {
+//                         e.preventDefault();
+//                         handleLinkClick("2-2", "tutorial-2");
+//                       }}
+//                     >
+//                       Repetir tutorial
+//                     </Link>
+//                   </li>
+//                   <li className="ml-2">
+//                     <Link
+//                       to="#tutorial-3"
+//                       className="text-[1.1rem] hover:underline"
+//                       onClick={(e) => {
+//                         e.preventDefault();
+//                         handleLinkClick("2-3", "tutorial-3");
+//                       }}
+//                     >
+//                       Cria conta CT-HC
+//                     </Link>
+//                   </li>
+//                   <li className="ml-2">
+//                     <Link
+//                       to="#tutorial-4"
+//                       className="text-[1.1rem] hover:underline"
+//                       onClick={(e) => {
+//                         e.preventDefault();
+//                         handleLinkClick("2-4", "tutorial-4");
+//                       }}
+//                     >
+//                       Uso em dispositivos móveis
+//                     </Link>
+//                   </li>
+//                 </ul>
+//               </nav>
+//             </div>
+
+//             <div
+//               className="bg-[var(--color-white)] flex flex-col items-center w-[60%]
+//             border-[4px] border-[var(--color-blue-2)] rounded-[20px] h-full"
+//             >
+//               <h3 className="text-3xl w-[75%] text-center font-bold text-[var(--dark-blue-title)]">
+//                 Tem alguma outra pergunta? Mande para nós!
+//               </h3>
+
+//               <div className="flex flex-row justify-center items-center">
+//                 <form
+//                   method="POST"
+//                   className="flex flex-col h-fit justify-center gap-10"
+//                 >
+//                   <label htmlFor="name">Nome:</label>
+//                   <input
+//                     type="text"
+//                     placeholder="Digite seu nome"
+//                     className="w-[70%] px-4 py-3 rounded-[20px]
+//                 bg-gradient-to-b from-[var(--color-white)] to-gray-200 border-b-3 border-[var(--dark-blue-title)]
+//                 placeholder-[--color-gray] placeholder:font-semibold outline-none"
+//                   />
+
+//                   <label htmlFor="e-mail">E-mail:</label>
+//                   <input
+//                     type="text"
+//                     placeholder="Digite seu e-mail"
+//                     className="w-[70%] px-4 py-3 rounded-[20px]
+//                 bg-gradient-to-b from-[var(--color-white)] to-gray-200 border-b-3 border-[var(--dark-blue-title)]
+//                 placeholder-[--color-gray] placeholder:font-semibold outline-none"
+//                   />
+
+//                   <label htmlFor="pergunta">Qual a sua pergunta?</label>
+//                   <textarea
+//                     placeholder="Digite sua mensagem..."
+//                     className="w-[70%] px-4 py-3 rounded-[20px]
+//                 bg-gradient-to-b from-[var(--color-white)] to-gray-200 border-b-3 border-[var(--dark-blue-title)]
+//                 placeholder-[--color-gray] placeholder:font-semibold outline-none"
+//                   />
+
+//                   <button className="bg-[var(--color-blue-2)] p-2 w-[25%] text-[var(--color-white)] rounded-full font-bold text-2xl cursor-pointer shadow-[4px_4px_15px_var(--color-blue)] hover:bg-[var(--hover-button)] transition-colors duration-300">
+//                     Enviar
+//                   </button>
+//                 </form>
+//                 <figcaption>
+//                   <img
+//                     src="./question-img.png"
+//                     alt="Pessoa fazendo uma pergunta."
+//                   />
+//                 </figcaption>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//       <section className="min-h-screen px-10 pt-30 lg:px-20 xl:px-43">
+//         <div className="flex flex-col gap-32 py-40">
+//           {/* Categoria: Sobre o projeto */}
+//           <ul className="flex flex-col gap-8">
+//             <li>
+//               <h3 className="text-4xl text-[var(--dark-blue-title)] font-bold mb-3">Sobre o projeto</h3>
+//               <hr className="border-b-4 border-[var(--dark-blue-title)]"/>
+//             </li>
+
+//             {faqData[0].itens.map((item, index) => {
+//               const itemId = `0-${index}`;
+//               const isOpen = openItem === itemId;
+
+//               return (
+//                 <li
+//                   key={index}
+//                   id={`sobre-${index}`}
+//                   className="
+//                   flex flex-col bg-[var(--color-blue)] rounded-[20px] border-[var(--color-blue-2)] border-b-5 px-3 py-2 
+//                   cursor-pointer transition-all duration-300 text-[var(--color-white)]
+//                   hover:bg-[var(--color-blue-2)]
+//                   "
+//                   onClick={() => toggleItem(itemId)}
+//                 >
+//                   <div className="flex items-center justify-between">
+//                     <p className="font-bold text-2xl text-[var(--color-white)]">
+//                       {item.pergunta}
+//                     </p>
+//                     <div
+//                       className={`
+//                       mr-4 p-2 bg-[var(--color-blue)] rounded-full 
+//                       transition-transform duration-300 hover:bg-[var(--color-blue-2)] ${isOpen ? 'rotate-180' : ''}
+//                       `}>
+//                       <FaAngleDown className="text-[var(--color-white)] text-4xl"/>
+//                     </div>
+//                   </div>
+
+//                   {/* Resposta que aparece quando o item está aberto */}
+//                   <div
+//                     className={`
+//                     overflow-hidden transition-all duration-500 ease-in-out 
+//                     ${isOpen ? 'max-h-96 mt-4' : 'max-h-0'}
+//                     `}>
+//                     <div className="p-4 bg-[var(--light-blue-2)] rounded-2xl text-[var(--dark-blue-title)]">
+//                       <p className="text-lg font-bold">{item.resposta}</p>
+//                     </div>
+//                   </div>
+//                 </li>
+//               );
+//             })}
+//           </ul>
+
+//           {/* Categoria: Dados e Segurança */}
+//           <ul className="flex flex-col gap-8">
+//             <li>
+//               <h4 className="text-4xl text-[var(--dark-blue-title)] font-bold mb-3">Dados e Segurança</h4>
+//               <hr className="border-b-4 border-[var(--dark-blue-title)]"/>
+//             </li>
+
+//             {faqData[1].itens.map((item, index) => {
+//               const itemId = `1-${index}`;
+//               const isOpen = openItem === itemId;
+
+//               return (
+//                 <li
+//                   key={index}
+//                   id={`dados-${index}`}
+//                   className="
+//                   flex flex-col bg-[var(--color-blue)] rounded-[20px] border-[var(--color-blue-2)] border-b-5 px-3 py-2 
+//                   cursor-pointer transition-all duration-300 text-[var(--color-white)]
+//                   hover:bg-[var(--color-blue-2)]
+//                   "
+//                   onClick={() => toggleItem(itemId)}
+//                 >
+//                   <div className="flex items-center justify-between">
+//                     <p className="font-bold text-2xl text-[var(--color-white)]">
+//                       {item.pergunta}
+//                     </p>
+//                     <div
+//                       className={`
+//                       mr-4 p-2 bg-[var(--color-blue)] rounded-full 
+//                       transition-transform duration-300 hover:bg-[var(--color-blue-2)] ${isOpen ? 'rotate-180' : ''}
+//                       `}>
+//                       <FaAngleDown className="text-[var(--color-white)] text-4xl"/>
+//                     </div>
+//                   </div>
+
+//                   {/* Resposta que aparece quando o item está aberto */}
+//                   <div
+//                     className={`
+//                     overflow-hidden transition-all duration-500 ease-in-out 
+//                     ${isOpen ? 'max-h-96 mt-4' : 'max-h-0'}
+//                     `}>
+//                     <div className="p-4 bg-[var(--light-blue-2)] rounded-2xl text-[var(--dark-blue-title)]">
+//                       <p className="text-lg font-bold">{item.resposta}</p>
+//                     </div>
+//                   </div>
+//                 </li>
+//               );
+//             })}
+//           </ul>
+
+//           {/* Categoria: Uso do Tutorial */}
+//           <ul className="flex flex-col gap-8">
+//             <li>
+//               <h5 className="text-4xl text-[var(--dark-blue-title)] font-bold mb-3">Uso do Tutorial</h5>
+//               <hr className="border-b-4 border-[var(--dark-blue-title)]"/>
+//             </li>
+
+//             {faqData[2].itens.map((item, index) => {
+//               const itemId = `2-${index}`;
+//               const isOpen = openItem === itemId;
+
+//               return (
+//                 <li
+//                   key={index}
+//                   id={`tutorial-${index}`}
+//                   className="
+//                   flex flex-col bg-[var(--color-blue)] rounded-[20px] border-[var(--color-blue-2)] border-b-5 px-3 py-2 
+//                   cursor-pointer transition-all duration-300 text-[var(--color-white)]
+//                   hover:bg-[var(--color-blue-2)]
+//                   "
+//                   onClick={() => toggleItem(itemId)}
+//                 >
+//                   <div className="flex items-center justify-between">
+//                     <p className="font-bold text-2xl text-[var(--color-white)]">
+//                       {item.pergunta}
+//                     </p>
+//                     <div
+//                       className={`
+//                       mr-4 p-2 bg-[var(--color-blue)] rounded-full 
+//                       transition-transform duration-300 hover:bg-[var(--color-blue-2)] ${isOpen ? 'rotate-180' : ''}
+//                       `}>
+//                       <FaAngleDown className="text-[var(--color-white)] text-2xl"/>
+//                     </div>
+//                   </div>
+
+//                   {/* Resposta que aparece quando o item está aberto */}
+//                   <div
+//                     className={`
+//                     overflow-hidden transition-all duration-500 ease-in-out 
+//                     ${isOpen ? 'max-h-96 mt-4' : 'max-h-0'}
+//                     `}>
+//                     <div className="p-4 bg-[var(--light-blue-2)] rounded-2xl text-[var(--dark-blue-title)]">
+//                       <p className="text-lg font-bold">{item.resposta}</p>
+//                     </div>
+//                   </div>
+//                 </li>
+//               );
+//             })}
+//           </ul>
+//         </div>
+//       </section>
+//     </main>
+//   );
+// }
