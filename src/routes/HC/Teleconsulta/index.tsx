0bb4597f9683d0d84488cb59fa3a94e974699615
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-26 10:47:13 -0300

    feat(Teleconsulta): feito a criação do conteudo dinâmico

--- src/routes/HC/Teleconsulta/index.tsx
+++ src/routes/HC/Teleconsulta/index.tsx
@@ -1,7 +1,15 @@
+import ConteudoDinamico from "../../../components/HC/ConteudoDinamico";
 import TitleHC from "../../../components/HC/TitleHC";
 
 export default function Teleconsulta() {
     return (
-        <TitleHC title="Teleconsulta"/>
+        <section className="flex flex-col min-h-full">
+            <TitleHC title="Teleconsulta" />
+
+            <ConteudoDinamico>
+                Você não possui agendamentos de teleconsulta no momento
+            </ConteudoDinamico>
+        </section>
+
     )
 }
