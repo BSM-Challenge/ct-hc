7223dd2bc9877867390e5dc1375cff0d08dbd4d4
Sofia Siqueira <ssofiasiq@gmail.com>
2025-10-27 11:10:58 -0300

    enhancement (iconsTreinamento): Excluindo icons de treinamento e substituindo por URLs.

--- src/data/HC/iconsTreinamento.ts
+++ src/data/HC/iconsTreinamento.ts
@@ -1,11 +1,11 @@
 import type { IconsTreinamentoProps } from "../../types/HC/iconTreinamento";
 
 export const iconsTreinamento: IconsTreinamentoProps[] = [
-    {img: 'icon-resultados-azul.png', link: '', label: 'Meus Resultados', titleMessage: 'Clique para iniciar o treinamento dos meus resultados'},
-    {img: 'icon-receitas-azul.png', link: '', label: 'Minhas Receitas', titleMessage: 'Clique para iniciar o treinamento das minhas receitas'},
-    {img: 'icon-agendamentos-azul.png', link: '', label: 'Minhas Agendas', titleMessage: 'Clique para iniciar o treinamento dos meus agendamentos'},
-    {img: 'icon-teleconsulta-azul.png', link: '', label: 'Teleconsulta', titleMessage: 'Clique para iniciar o treinamento da teleconsulta'},
-    {img: 'icon-exames-azul.png', link: '', label: 'Solicitação de Exames', titleMessage: 'Clique para iniciar o treinamento de solicitação de exames'},
-    {img: 'icon-documentos-azul.png', link: '', label: 'Documentos', titleMessage: 'Clique para iniciar o treinamento de documentos'},
-    {img: 'icon-ajuda-azul.png', link: '', label: 'Preciso de Ajuda', titleMessage: 'Clique para iniciar o treinamento de preciso de ajuda'},
+    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573769/icon-resultados-azul_l0oaoj.png', link: '', label: 'Meus Resultados', titleMessage: 'Clique para iniciar o treinamento dos meus resultados'},
+    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573767/icon-receitas-azul_gjh9sn.png', link: '', label: 'Minhas Receitas', titleMessage: 'Clique para iniciar o treinamento das minhas receitas'},
+    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573740/icon-agendamentos-azul_gukt1g.png', link: '', label: 'Minhas Agendas', titleMessage: 'Clique para iniciar o treinamento dos meus agendamentos'},
+    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573807/icon-teleconsulta-azul_ba4bcp.png', link: '', label: 'Teleconsulta', titleMessage: 'Clique para iniciar o treinamento da teleconsulta'},
+    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573789/icon-exames-azul_fdzp1u.png', link: '', label: 'Solicitação de Exames', titleMessage: 'Clique para iniciar o treinamento de solicitação de exames'},
+    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573787/icon-documentos-azul_lgdro7.png', link: '', label: 'Documentos', titleMessage: 'Clique para iniciar o treinamento de documentos'},
+    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573739/icon-ajuda-azul_gggw6t.png', link: '', label: 'Preciso de Ajuda', titleMessage: 'Clique para iniciar o treinamento de preciso de ajuda'},
 ]
