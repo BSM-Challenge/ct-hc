764520dbe4316073bc79b14ba18a81761aba4a52
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-28 09:07:53 -0300

    feat(Documentos): criado a página de documentos
    E foi adicionado um campo dinâmico

--- src/routes/HC/Documentos/index.tsx
+++ src/routes/HC/Documentos/index.tsx
@@ -2,8 +2,11 @@ import TitleHC from "../../../components/HC/TitleHC";
 
 export default function Documentos() {
     return (
-        <section>
+        <section className="flex flex-col min-h-full">
             <TitleHC title="Meus Documentos" />
+            <div className="flex justify-center items-center flex-grow">
+                <p>Nenhum documento disponível no momento.</p>
+            </div>
         </section>
     )
 }
