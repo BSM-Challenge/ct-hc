421b39f65499e09a7644a8a1b6ee3d432a2bedcf
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-28 09:03:18 -0300

    feat(Documentos): criado a estrutura base da página Documentos

---
+++ src/routes/HC/Documentos/index.tsx
@@ -0,0 +1,9 @@
+import TitleHC from "../../../components/HC/TitleHC";
+
+export default function Documentos() {
+    return (
+        <section>
+            <TitleHC title="Meus Documentos" />
+        </section>
+    )
+}
