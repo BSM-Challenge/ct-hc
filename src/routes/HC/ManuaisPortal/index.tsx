fd0a2e24b2a9833082c706e76bfc8364386c9b48
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-31 14:43:02 -0300

    feat(ManuaisPortal): criado a estrutura base do ManuaisPortal

---
+++ src/routes/HC/ManuaisPortal/index.tsx
@@ -0,0 +1,14 @@
+import TitleHC from "../../../components/HC/TitleHC";
+
+export default function ManuaisPortal() {
+    return (
+        <section>
+            <TitleHC 
+             title="Manuais do Portal" 
+             tamanho="2xl"
+             icon={true}
+             link="/hc/ajuda"
+            />
+        </section>
+    )
+}
