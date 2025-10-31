71300696ca95ee511c37bcf0dcb5ff9928fbd1fe
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-31 14:30:21 -0300

    feat(TitleHC): criado o link de voltar
    Criado um link para voltar para a página anterior

--- src/components/HC/TitleHC/index.tsx
+++ src/components/HC/TitleHC/index.tsx
@@ -1,8 +1,20 @@
+import { IoMdArrowBack } from "react-icons/io";
 import type { TitleHCProps } from "../../../types/HC/title";
+import { Link } from "react-router-dom";
 
-export default function TitleHC( {title, subtitle, tamanho='2xl', font='normal', line = false}: TitleHCProps) {
+export default function TitleHC( {title, subtitle, tamanho='2xl', font='normal', line = false, icon = false, link = "/"}: TitleHCProps) {
     return (
-        <div>
+        <div className="flex items-center gap-4">
+            { icon && 
+            <Link 
+            to={link}
+            title="Voltar"
+            className="
+            p-2 hover:bg-[var(--color-blue-EAF3FC)] duration-200 rounded-full
+            ">
+                <IoMdArrowBack size={25} className="text-[var(--color-blue)] cursor-pointer"/> 
+            </Link>
+            }
             <h1 className={`text-${tamanho} text-[var(--color-blue)] font-${font}`}>{title}</h1>
             { line && <hr className="my-3"/> }
             <h2 className="text-lg">{subtitle}</h2>
