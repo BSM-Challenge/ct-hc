311ec6a5fb28decff484956342b85e98d7a158ec
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-31 17:03:37 -0300

    feat(data/pdfs): criado os dados dos manuais

---
+++ src/data/HC/pdfs.ts
@@ -0,0 +1,10 @@
+import type { PdfsProps } from "../../types/HC/pdfs";
+
+export const Pdfs: PdfsProps[] = [
+  { link: "/hc-manual-acesso-sistema-portal.pdf", text: "Manual de Acesso ao Portal do Paciente" },
+  { link: "/hc-manual-teleconsulta-portal.pdf", text: "Manual de Teleconsulta" },
+  { link: "/hc-manual-sistema-portal-app.pdf", text: "Manual do Portal do Paciente App" },
+  { link: "/hc-manual-sistema-portal-web.pdf", text: "Manual do Portal do Paciente Web" },
+  { link: "/hc-politica-privacidade-portal_v1.pdf", text: "Política de Privacidade" },
+  { link: "/hc-termo-consentimento-teleconsulta-portal_v1.pdf", text: "Termo de Consentimento da Teleconsulta" },
+];
