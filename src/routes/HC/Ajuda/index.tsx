0e716a96aeb2c390a40971d56490db7c50ee6e7c
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-30 23:34:10 -0300

    feat(Ajuda): criado a estrutura base da página de ajuda

---
+++ src/routes/HC/Ajuda/index.tsx
@@ -0,0 +1,9 @@
+import TitleHC from "../../../components/HC/TitleHC";
+
+export default function Ajuda() {
+    return (
+        <section>
+            <TitleHC title="Ajuda/ Manuais" />
+        </section>
+    )
+}
