eea1be0195d35ab9709da0135303051fd5ae744c
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-30 23:55:45 -0300

    feat(Ajuda): criado link para a página de manuais do portal

--- src/routes/HC/Ajuda/index.tsx
+++ src/routes/HC/Ajuda/index.tsx
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom";
 import TitleHC from "../../../components/HC/TitleHC";
 
 export default function Ajuda() {
     return (
-        <section>
+        <section className="flex flex-col gap-3">
             <TitleHC title="Ajuda/ Manuais" />
+            <div className="mx-10">
+                <Link 
+                to=""
+                title="Clique aqui para ver os manuais do portal"
+                className="
+                flex justify-center w-full py-1 rounded-full
+                text-[var(--color-blue-1CB1B1)] border-2 
+                hover:bg-[var(--color-cyan-hover)] duration-300
+                "
+                >
+                    Manuais do Portal
+                </Link>
+            </div>
         </section>
     )
 }
