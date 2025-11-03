8f555ca2e4ef7f251ed060d59525712c27ee43cc
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-03 15:51:24 -0300

    feat(ConteudoDinamico): adicionado a classe conteudo-dinamico

--- src/components/HC/ConteudoDinamico/index.tsx
+++ src/components/HC/ConteudoDinamico/index.tsx
@@ -2,7 +2,7 @@ import type { ConteudoDinamicoProps } from "../../../types/HC/conteudoDinamico";
 
 export default function ConteudoDinamico({children}: ConteudoDinamicoProps) {
     return (
-     <div className="w-full flex justify-center flex-grow items-center">
+     <div className="conteudo-dinamico w-full flex justify-center flex-grow items-center">
         <p className="text-center">
             {children}
         </p>
