e85f2ded4b955c126d186aea96693e85cfe5ee8e
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-03 16:07:46 -0300

    feat(Teleconsulta): adicionado os steps na página

--- src/routes/HC/Teleconsulta/index.tsx
+++ src/routes/HC/Teleconsulta/index.tsx
@@ -1,10 +1,21 @@
 import ConteudoDinamico from "../../../components/HC/ConteudoDinamico";
 import TitleHC from "../../../components/HC/TitleHC";
+import TutorialHC from "../../../components/HC/TutorialHC";
 
 export default function Teleconsulta() {
+
+    const steps = [
+        {
+            target: ".conteudo-dinamico",
+            content: "Aqui aonde vai aparecer suas teleconsultas"
+        }
+    ]
+
     return (
         <section className="flex flex-col min-h-full">
             <TitleHC title="Teleconsulta" />
+
+            <TutorialHC steps={steps} />
 
             <ConteudoDinamico>
                 Você não possui agendamentos de teleconsulta no momento
