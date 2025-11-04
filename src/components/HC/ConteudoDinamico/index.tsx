24bfbec6dff033f36214f8b3b042d24373505fec
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-04 11:04:59 -0300

    feat(ConteudoDinamico): adicionado a classe conteudo-dinamico-resultados

--- src/components/HC/ConteudoDinamico/index.tsx
+++ src/components/HC/ConteudoDinamico/index.tsx
@@ -2,7 +2,10 @@ import type { ConteudoDinamicoProps } from "../../../types/HC/conteudoDinamico";
 
 export default function ConteudoDinamico({children}: ConteudoDinamicoProps) {
     return (
-     <div className="conteudo-dinamico w-full flex justify-center flex-grow items-center">
+     <div className="
+     conteudo-dinamico-resultados
+     conteudo-dinamico w-full flex justify-center flex-grow items-center
+     ">
         <p className="text-center">
             {children}
         </p>
