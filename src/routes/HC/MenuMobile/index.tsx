dcb0cc480c5a8f3c1b7844b26dff2ef29bbbe445
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-05 08:37:31 -0300

    feat(MenuMobile): criado o menu mobile

--- src/routes/HC/MenuMobile/index.tsx
+++ src/routes/HC/MenuMobile/index.tsx
@@ -1,7 +1,48 @@
+import { IoPersonCircle } from "react-icons/io5";
+import { Pdfs } from "../../../data/HC/pdfs";
+import { MdPictureAsPdf } from "react-icons/md";
+import { menuItems } from "../../../data/HC/menuItem";
+import { Link } from "react-router-dom";
+
 export default function MenuMobile() {
     return (
         <section>
-            menu mobile
+            <header className="flex justify-between items-center">
+                <img 
+                src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1761431082/logo-hc_l7m35z.png" 
+                alt="Logo do Hospital das Clínicas"
+                className="w-[30%]"
+                />
+                <IoPersonCircle className="text-[var(--color-blue)] text-4xl" />
+            </header>
+             <ul className="flex flex-wrap px-5 gap-4 max-lg:justify-center">
+                {menuItems.map((menu, index) => (
+                <li key={index}>
+                    <Link to={menu.to} title="Clique aqui para acessar o manual">
+                        <figure
+                        className="
+                            flex flex-col items-center justify-center gap-2
+                            border border-[var(--pdf-color)]
+                            p-8 w-[13rem] h-[13rem]
+                            rounded-4xl text-center
+                            hover:bg-[var(--color-grey-hover-2)]
+                            duration-300
+                            max-sm:p-2
+                        "
+                        >
+                        <img 
+                        src={menu.icon}
+                        alt=""
+                        className="w-[30%]"
+                        />
+                        <figcaption className="text-sm font-normal leading-snug">
+                            {menu.label}
+                        </figcaption>
+                        </figure>
+                    </Link>
+                </li>
+                ))}
+            </ul>
         </section>
     )
 }
