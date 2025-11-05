b24c1fd37cfce52200e38f1148e226e081f50317
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-05 09:34:00 -0300

    fix(CentroDeTreinamento): removido a / do caminho do img
    Pois estava impedindo das imagens aparecerem

--- src/routes/HC/CentroDeTreinamento/index.tsx
+++ src/routes/HC/CentroDeTreinamento/index.tsx
@@ -5,7 +5,7 @@ import CardExperiencia from "../../../components/HC/CardExperiencia";
 
 export default function CentroDeTreinamento() {
     return (
-        <section className="flex flex-col gap-8 max-lg:gap-10">
+        <section className="flex flex-col gap-6 max-lg:gap-10">
             <TitleHC
                 title="Centro de Treinamento"
                 subtitle="Escolha a área do site que deseja fazer o treinamento e receba instruções em cada etapa:"
@@ -32,7 +32,7 @@ export default function CentroDeTreinamento() {
                     py-4 min-w-[200px] 
                     ">
                         <figure className="flex flex-col items-center gap-2">
-                            <img src={`/${i.img}`} alt="" className="w-[4rem] lg:w-[3rem]" />
+                            <img src={`${i.img}`} alt="" className="w-[4rem] lg:w-[3rem]" />
                             <figcaption className="w-[70%] text-center font-bold text-lg lg:text-[18px]">
                             {i.label}
                             </figcaption>
