2f5c213a66b76694d0169422ddf06acdc14b7fc0
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-05 08:46:22 -0300

    feat(Avisos): criado a página de avisos

--- src/routes/HC/AvisosMobile/index.tsx
+++ src/routes/HC/AvisosMobile/index.tsx
@@ -1,7 +1,16 @@
+import ConteudoDinamico from "../../../components/HC/ConteudoDinamico";
+
 export default function AvisosMobile() {
     return (
-        <section>
-            avisos
+        <section className="flex flex-col min-h-screen">
+            <header className="flex justify-center">
+                <h1 className="text-[var(--color-blue-0077C8)] text-2xl">
+                    Histórico
+                </h1>
+            </header>
+            <ConteudoDinamico>
+                Não há nenhum aviso no momento.
+            </ConteudoDinamico>
         </section>
     )
 }
