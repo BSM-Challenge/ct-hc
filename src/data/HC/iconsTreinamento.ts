258398cf0e8af5aab4140b24e3bd80dd29de352c
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-05 23:32:54 -0300

    feat(data/iconsTreinamento): adicionado as rotas para as páginas

--- src/data/HC/iconsTreinamento.ts
+++ src/data/HC/iconsTreinamento.ts
@@ -1,11 +1,53 @@
 import type { IconsTreinamentoProps } from "../../types/HC/iconTreinamento";
 
 export const iconsTreinamento: IconsTreinamentoProps[] = [
-    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573769/icon-resultados-azul_l0oaoj.png', link: '', label: 'Meus Resultados', titleMessage: 'Clique para iniciar o treinamento dos meus resultados'},
-    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573767/icon-receitas-azul_gjh9sn.png', link: '', label: 'Minhas Receitas', titleMessage: 'Clique para iniciar o treinamento das minhas receitas'},
-    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573740/icon-agendamentos-azul_gukt1g.png', link: '', label: 'Minhas Agendas', titleMessage: 'Clique para iniciar o treinamento dos meus agendamentos'},
-    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573807/icon-teleconsulta-azul_ba4bcp.png', link: '', label: 'Teleconsulta', titleMessage: 'Clique para iniciar o treinamento da teleconsulta'},
-    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573789/icon-exames-azul_fdzp1u.png', link: '', label: 'Solicitação de Exames', titleMessage: 'Clique para iniciar o treinamento de solicitação de exames'},
-    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573787/icon-documentos-azul_lgdro7.png', link: '', label: 'Documentos', titleMessage: 'Clique para iniciar o treinamento de documentos'},
-    {img: 'https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573739/icon-ajuda-azul_gggw6t.png', link: '', label: 'Preciso de Ajuda', titleMessage: 'Clique para iniciar o treinamento de preciso de ajuda'},
-]
+  {
+    img: "https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573769/icon-resultados-azul_l0oaoj.png",
+    link: "/hc/resultados",
+    label: "Meus Resultados",
+    titleMessage: "Clique para iniciar o treinamento dos meus resultados",
+    modoTreinamento: "resultados",
+  },
+  {
+    img: "https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573767/icon-receitas-azul_gjh9sn.png",
+    link: "/hc/receitas",
+    label: "Minhas Receitas",
+    titleMessage: "Clique para iniciar o treinamento das minhas receitas",
+    modoTreinamento: "receitas",
+  },
+  {
+    img: "https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573740/icon-agendamentos-azul_gukt1g.png",
+    link: "/hc/agenda",
+    label: "Minhas Agendas",
+    titleMessage: "Clique para iniciar o treinamento dos meus agendamentos",
+    modoTreinamento: "agenda",
+  },
+  {
+    img: "https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573807/icon-teleconsulta-azul_ba4bcp.png",
+    link: "/hc/teleconsulta",
+    label: "Teleconsulta",
+    titleMessage: "Clique para iniciar o treinamento da teleconsulta",
+    modoTreinamento: "teleconsulta",
+  },
+  {
+    img: "https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573789/icon-exames-azul_fdzp1u.png",
+    link: "/hc/exames",
+    label: "Solicitação de Exames",
+    titleMessage: "Clique para iniciar o treinamento de solicitação de exames",
+    modoTreinamento: "exames",
+  },
+  {
+    img: "https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573787/icon-documentos-azul_lgdro7.png",
+    link: "/hc/documentos",
+    label: "Documentos",
+    titleMessage: "Clique para iniciar o treinamento de documentos",
+    modoTreinamento: "documentos",
+  },
+  {
+    img: "https://res.cloudinary.com/dt26mfzpw/image/upload/v1761573739/icon-ajuda-azul_gggw6t.png",
+    link: "/hc/ajuda",
+    label: "Preciso de Ajuda",
+    titleMessage: "Clique para iniciar o treinamento de preciso de ajuda",
+    modoTreinamento: "ajuda",
+  },
+];
