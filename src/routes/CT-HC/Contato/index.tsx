62155cc594fd63fcb011d67d5109bf0f6fb896da
Sofia Siqueira <ssofiasiq@gmail.com>
2025-11-06 12:18:36 -0300

    refactor(Contato): refeita grande parte da responsividade do formulário de contato e do card de Mensagem Enviada para todos os dispositivos.

--- src/routes/CT-HC/Contato/index.tsx
+++ src/routes/CT-HC/Contato/index.tsx
@@ -57,7 +57,7 @@ export default function Contato() {
 
       <div className="flex flex-col justify-center my-10
       sm:flex-col lg:flex-row 2xl:h-[100%] 2xl:w-[100%]">
-        <section className="bg-[var(--light-blue-2)] rounded-3xl p-6 flex-1 w-[100%] shadow-xl/20 mb-10
+        <section className="bg-[var(--light-blue-2)] rounded-3xl p-3 flex-1 w-[100%] shadow-xl/20 mb-10
         sm:w-[80%] sm:rounded-3xl sm:self-center sm:mb-10
         lg:rounded-br-none lg:rounded-tr-none lg:mb-0 lg:w-[100%]">
           <div className="flex flex-col sm:ml-4 md:items-center lg:px-1 lg:py-6 lg:items-start">
@@ -113,7 +113,7 @@ export default function Contato() {
             </div>
 
             {/* redes sociais */}
-            <hr className="border-[var(--dark-blue-title)] w-70 border-2 mb-2 self-center md:w-[90%] lg:mt-5" />
+            <hr className="border-[var(--dark-blue-title)] w-[90%] border-2 mb-2 self-center md:w-[90%] lg:mt-5" />
 
             <div className="flex flex-col items-center gap-1
             sm:self-center sm:mr-4">
@@ -154,55 +154,71 @@ export default function Contato() {
           </div>
         </section>
 
-        <section className="bg-[var(--color-white)] rounded-3xl w-[100%] shadow-xl/20
+        <section className="bg-[var(--color-white)] rounded-3xl shadow-xl/20
         sm:w-[80%] sm:rounded-3xl sm:self-center sm:mb-10
-        lg:rounded-bl-none lg:rounded-tl-none lg:mb-0 lg:w-1/2">
+        lg:rounded-l-none lg:mb-0
+        lg:w-1/2 lg:h-[550px]
+        xl:h-[620px]">
 
-          <div className="flex flex-col items-center w-[100%] h-[100%] pt-6 sm:items-center xl:py-13">
-            
-
+          <div className="flex flex-col justify-center items-center w-[100%] h-[100%] sm:mt-5 xl:py-9">
             {!mensagem ? (
-              <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-[80%] h-[100%]
+              <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center w-[80%] h-[100%]
               sm:w-[80%] md:w-[70%]
               lg:py-0 lg:px-10 lg:w-[100%]
-              xl:w-[90%]">
+              xl:w-[75%]">
                 <h2 className="text-[var(--dark-blue-title)] text-4xl text-center font-bold mb-8
-                sm:text-center sm:text-3xl
+                sm:text-3xl
                 md:text-4xl
-                lg:self-center lg:mb-5">
+                lg:text-3xl lg:mb-4">
                   Mande uma mensagem!
                 </h2>
 
                 <label htmlFor="nome" className="text-[var(--dark-blue-title)] font-bold text-3xl mb-1">Nome:</label>
-                <input type="text" placeholder="Digite o seu nome" className="bg-[var(--light-blue)] w-[100%] placeholder:text-[var(--dark-blue-2)] placeholder:font-bold rounded-md py-[5px] pl-3 mb-5 border-b-4 border-[var(--dark-blue-title)]" {...register("nome")} />
-                {errors.nome && (<p className="text-red-500 font-semibold text-sm">{errors.nome.message}</p>)}
+                <input type="text" placeholder="Digite o seu nome" className="bg-[var(--light-blue)] w-[100%] placeholder:text-[var(--dark-blue-2)] placeholder:font-bold rounded-md py-[5px] pl-3 mb-2 border-b-4 border-[var(--dark-blue-title)] lg:mb-5" {...register("nome")} />
+                {errors.nome && (<p className="text-red-500 font-semibold text-sm mb-7
+                lg:mb-2">{errors.nome.message}</p>)}
 
                 <label htmlFor="email" className="text-[var(--dark-blue-title)] font-bold text-3xl mb-1">E-mail:</label>
-                <input type="email" placeholder="Digite o seu e-mail" className="bg-[var(--light-blue)] placeholder:text-[var(--dark-blue-2)] placeholder:font-bold rounded-md py-[5px] pl-3 mb-5 border-b-4 border-[var(--dark-blue-title)]" {...register("email")} />
-                {errors.email && (<p className="text-red-500 font-semibold text-sm">{errors.email.message}</p>)}
+                <input type="email" placeholder="Digite o seu e-mail" className="bg-[var(--light-blue)] placeholder:text-[var(--dark-blue-2)] placeholder:font-bold rounded-md py-[5px] pl-3 mb-2 border-b-4 border-[var(--dark-blue-title)] lg:mb-5" {...register("email")} />
+                {errors.email && (<p className="text-red-500 font-semibold text-sm mb-7
+                lg:mb-2">{errors.email.message}</p>)}
 
                 <label htmlFor="mensagem" className="text-[var(--dark-blue-title)] font-bold text-3xl mb-1">Mensagem:</label>
                 <textarea placeholder="Digite sua mensagem..."
-                  className="bg-[var(--light-blue)] placeholder:text-[var(--dark-blue-2)] placeholder:font-bold rounded-md py-[5px] pl-3 mb-8 w-[100%] h-40 resize-none border-b-4 border-[var(--dark-blue-title)] lg:h-40 lg:mb-5 xl:mb-8" {...register("mensagem")} />
-                {errors.mensagem && (<p className="text-red-500 font-semibold text-sm">{errors.mensagem.message}</p>)}
+                  className="bg-[var(--light-blue)] placeholder:text-[var(--dark-blue-2)] placeholder:font-bold
+                  rounded-md py-[5px] pl-3 mb-2 w-[100%] h-40 resize-none border-b-4 border-[var(--dark-blue-title)]
+                  lg:h-25
+                  xl:h-44" {...register("mensagem")} />
+                {errors.mensagem && (<p className="text-red-500 font-semibold text-sm mb-7
+                lg:mb-2">{errors.mensagem.message}</p>)}
 
-                <button type="submit" className="text-[var(--dark-blue-title)] text-2xl font-bold border-4 border-[var(--dark-blue-title)] rounded-xl p-2 w-[45%] mb-5 self-center hover:bg-[var(--light-blue)] hover:text-[var(--color-blue-2)] hover:border-[var(--color-blue-2)] cursor-pointer transition-colors duration-300 xl:mb-0 xl:text-3xl"> {carregando ? "Enviando..." : "Enviar"}</button>
+                <button type="submit"
+                className="text-[var(--dark-blue-title)] text-2xl font-bold border-4 border-[var(--dark-blue-title)]
+                rounded-xl p-2 w-[45%] mb-5 self-center hover:bg-[var(--light-blue)] hover:text-[var(--color-blue-2)]
+                hover:border-[var(--color-blue-2)] cursor-pointer transition-colors duration-300
+                xl:mb-0 xl:text-3xl xl:w-[35%]"> {carregando ? "Enviando..." : "Enviar"}</button>
               </form>
             ) : (
-              <div className="flex flex-col justify-center items-center gap-10 px-10 py-7 font-bold bg-[#FFFFFF] rounded-2xl">
-                <h4 className="text-4xl text-center text-[var(--dark-blue-title)]
-                lg:text-3xl lg:w-full">
+              <div className="rounded-3xl flex flex-col p-8
+              lg:rounded-r-none lg:mb-8 lg:w-[100%]
+              xl:p-15
+              2xl:p-25">
+                <h4 className="text-4xl text-center font-semibold text-[var(--dark-blue-title)]
+                lg:text-3xl lg:w-full
+                xl:text-4xl">
                   Mensagem enviada com sucesso! Obrigado pelo contato.
                 </h4>
                 <span className="flex justify-center items-center">
                   <img
                   src="https://res.cloudinary.com/dtbgsboo5/image/upload/v1761775582/icon-check_ao22ng.png"
                   alt="Imagem de check com a cor verde"
-                  className="w-[60%]"/>
+                  className="w-[70%] sm:[60%] md:w-[50%] xl:w-[40%] my-7 xl:my-12"/>
                 </span>
-                <h5 className="text-3xl text-center">Deseja fazer outra pergunta?</h5>
-                <ul className="flex flex-col justify-center items-center w-full gap-5
-                md:flex-row">
+                <h5 className="text-3xl text-center text-[var(--dark-blue-title)]
+                xl:text-4xl">Deseja fazer outra pergunta?</h5>
+                <ul className="flex flex-col justify-center items-center w-full gap-6 mt-3
+                sm:flex-row
+                xl:mt-5 xl:gap-15">
                   <li>
                     <button
                       title="Clique aqui para voltar"
@@ -224,7 +240,7 @@ export default function Contato() {
                     <button
                       title="Clique aqui para fazer outra pergunta"
                       onClick={fazerOutraPergunta}
-                      className="px-7 py-3 border-3  rounded-[10px]
+                      className="px-7 py-3 rounded-[10px] border-4 border-[var(--dark-blue-title)]
                       bg-[var(--dark-blue-title)] text-[#FFFFFF] text-lg cursor-pointer
                       hover:bg-[var(--hover-button)] duration-500
                       max-[870px]:w-full">
@@ -232,7 +248,8 @@ export default function Contato() {
                     </button>
                   </li>
                 </ul>
-              </div>
+              
+            </div>
             )}
           </div>
         </section>
