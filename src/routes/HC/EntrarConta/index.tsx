75ccee080c329659cc21c4a883525e6895054303
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-09 17:24:07 -0300

    feat(EntrarConta): somente é possível fazer login se estiver na lista de usuários do LocalStorage

--- src/routes/HC/EntrarConta/index.tsx
+++ src/routes/HC/EntrarConta/index.tsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 
 const schema = z.object({
   emailOuCpf: z
@@ -18,6 +19,9 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>;
 
 export default function EntrarConta() {
+  const navigate = useNavigate();
+  const [mensagemErro, setMensagemErro] = useState("");
+
   const {
     register,
     handleSubmit,
@@ -25,23 +29,39 @@ export default function EntrarConta() {
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FormData) => {
-    console.log("Dados validados:", data);
-    // Jogar a api aqui
+    const usuarioSalvo = localStorage.getItem("usuarioHC");
+
+    if (!usuarioSalvo) {
+      setMensagemErro("Nenhuma conta encontrada. Crie uma conta primeiro!");
+      return;
+    }
+
+    const usuario = JSON.parse(usuarioSalvo);
+
+    const loginCorreto =
+      (usuario.email === data.emailOuCpf ||
+        usuario.cpf.replace(/\D/g, "") === data.emailOuCpf.replace(/\D/g, "")) &&
+      usuario.senha === data.senha;
+
+    if (loginCorreto) {
+      setMensagemErro("");
+      navigate("/hc");
+    } else {
+      setMensagemErro("E-mail/CPF ou senha incorreto.");
+    }
   };
 
   return (
     <section className="w-full min-h-screen bg-background-linear flex justify-center items-center px-5 ">
       <div className="bg-[var(--color-white-button-hover)] rounded-[20px] border-2 border-[var(--color-white)] px-6 py-6">
         <span className="flex justify-center mb-3 md:mb-5">
-          <img 
-          src="logo-hc-verde.png"
-          alt="Logo do HC"
-          className="
-          w-full
-          max-2xl:w-[70%]" 
-          
+          <img
+            src="logo-hc-verde.png"
+            alt="Logo do HC"
+            className="w-full max-2xl:w-[70%]"
           />
         </span>
+
         <span className="flex flex-col items-center gap-3">
           <h1 className="text-[var(--color-white)] text-center text-4xl font-bold md:text-5xl">
             Bem vindo de volta!
@@ -50,9 +70,14 @@ export default function EntrarConta() {
             Insira seus dados para continuar.
           </p>
         </span>
+
         <div className="flex flex-col items-center">
-          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-full">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col w-full"
+          >
             <div className="flex flex-col px-10 md:w-[100%]">
+              {/* E-mail ou CPF */}
               <label className="text-[var(--color-white)] text-3xl font-bold mb-3">
                 E-mail ou CPF:
               </label>
@@ -60,21 +85,21 @@ export default function EntrarConta() {
                 type="text"
                 {...register("emailOuCpf")}
                 placeholder="Digite seu E-mail ou CPF"
-                className={`mb-10 rounded-[20px] p-3
-                border-2 ${
-                    errors.emailOuCpf
-                      ? "border-[var(--color-red)]"
-                      : "border-[var(--color-blue-0077C8)]"
-                  } 
-                  bg-[var(--color-white-04)] 
-                  placeholder:text-[var(--color-white-075)] text-[18px] font-bold outline-none
-                  md:px-4`}
+                className={`mb-10 rounded-[20px] p-3 border-2 ${
+                  errors.emailOuCpf
+                    ? "border-[var(--color-red)]"
+                    : "border-[var(--color-blue-0077C8)]"
+                } 
+                bg-[var(--color-white-04)] 
+                placeholder:text-[var(--color-white-075)] text-[18px] font-bold outline-none md:px-4`}
               />
               {errors.emailOuCpf && (
                 <span className="text-[var(--color-red)] text-sm -mt-8 mb-5 ml-2">
                   {errors.emailOuCpf.message}
                 </span>
               )}
+
+              {/* Senha */}
               <label className="text-[var(--color-white)] text-3xl font-bold mb-3">
                 Senha:
               </label>
@@ -82,47 +107,51 @@ export default function EntrarConta() {
                 type="password"
                 {...register("senha")}
                 placeholder="Digite sua senha"
-                className={`mb-10 rounded-[20px] p-3
-                border-2 ${
-                    errors.senha
-                      ? "border-[var(--color-red)]"
-                      : "border-[var(--color-blue-0077C8)]"
-                  } 
-                  bg-[var(--color-white-04)] 
-                  placeholder:text-[var(--color-white-075)] text-[18px] font-bold outline-none
-                  md:px-4`}
+                className={`mb-10 rounded-[20px] p-3 border-2 ${
+                  errors.senha
+                    ? "border-[var(--color-red)]"
+                    : "border-[var(--color-blue-0077C8)]"
+                } 
+                bg-[var(--color-white-04)] 
+                placeholder:text-[var(--color-white-075)] text-[18px] font-bold outline-none md:px-4`}
               />
               {errors.senha && (
-                <span className="text-[var(--color-red)] text-sm -mt-8 mb-5 ml-2">
+                <span className="text-[var(--color-red)] text-sm -mt-8 ml-2">
                   {errors.senha.message}
                 </span>
               )}
             </div>
+            {mensagemErro && (
+              <p className="text-[var(--color-red)] text-center text-lg font-semibold -mt-5 mb-2">
+                {mensagemErro}
+              </p>
+            )}
 
             <div className="flex justify-end">
               <button
                 type="submit"
                 title="Clique aqui para entrar."
                 className="
-                    text-[var(--color-white)] px-12 py-2 flex justify-center
-                    border-2 border-[var(--color-white)] 
-                    rounded-[30px] text-[24px] cursor-pointer
-                    md:hover:bg-[var(--color-white-button-hover)] md:duration-300"
+                text-[var(--color-white)] px-12 py-2 
+                flex justify-center border-2 border-[var(--color-white)] 
+                rounded-[30px] text-[24px] cursor-pointer 
+                md:hover:bg-[var(--color-white-button-hover)] md:duration-300"
               >
                 Entrar
               </button>
             </div>
           </form>
         </div>
-            <Link
-              to="/criarConta"
-              title="Clique aqui para seguir para a página de criar conta."
-              className="text-[var(--color-white)] text-2xl"
-            >
-              Não tem uma conta?{" "}
-              <span className="font-bold md:hover:underline">Clique aqui.</span>
-            </Link>
-        </div>
+
+        <Link
+          to="/criarConta"
+          title="Clique aqui para seguir para a página de criar conta."
+          className="text-[var(--color-white)] text-2xl"
+        >
+          Não tem uma conta?{" "}
+          <span className="font-bold md:hover:underline">Clique aqui.</span>
+        </Link>
+      </div>
     </section>
   );
 }
